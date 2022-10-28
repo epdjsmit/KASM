613052707bdfe493ed99bb728205e3ec03d51f4c
ezaspy <ezaspython@gmail.com>
2022-10-28 16:53:25 -0400

    Delete scripts directory

--- scripts/cuckoo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python3 -m pip install setuptools, viper-framework, cuckoo
-mkdir /opt/cuckoo
-chown cuckoo:cuckoo /opt/cuckoo
-cuckoo --cwd /opt/cuckoo
-export CUCKOO=/opt/cuckoo
--- scripts/greenbone.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-add-apt-repository ppa:mrazavi/gvm && 
-apt-get update -y && apt-get upgrade -y && apt-get install net-tools gparted postgresql gvm openjdk-11-jdk elasticsearch kibana logstash -y
-greenbone-nvt-sync
-sudo su
-greenbone-scapdata-sync
-greenbone-certdata-sync
-echo 'OPTIONS="--listen=0.0.0.0 --port=9392"' > /etc/default/gsad
-systemctl restart gsad
