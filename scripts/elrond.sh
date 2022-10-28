616a2828c1be621ad3d0139914eb294bead1497c
ezaspy <ezaspython@gmail.com>
2022-10-28 22:02:56 +0100

    Add files via upload

---
+++ scripts/elrond.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo ./scripts/elrond/./init.sh
+sudo ./scripts/elrond/./sift.sh
+sudo ./scripts/elrond/./apfs-fuse.sh
+sudo ./scripts/elrond/./volatility3.sh
+sudo ./scripts/elrond/./dwarf2json.sh
+sudo ./scripts/elrond/./splunk.sh
+sudo ./scripts/elrond/./elastic.sh
+sudo ./scripts/elrond/./navigator.sh
