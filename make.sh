7431f318478f8448bbe316948a39399a27fcbfc9
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-10-29 14:10:59 -0400

    restructure

---
+++ make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /opt/elrond
+sudo chmod 744 -R /opt/elrond/
+sudo chown sansforensics:sansforensics -R /opt/elrond/
+export PATH=$PATH:/opt/elrond
+sudo chmod +x /opt/elrond/elrond/config.sh
+sudo chmod +x /opt/elrond/elrond/elrond.py
+clear
+cd /opt/elrond/elrond
+sudo ./config.sh
