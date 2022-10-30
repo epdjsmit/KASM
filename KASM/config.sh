57ae98071b8bc9fe732aec1af4ff6779adb87a66
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-10-30 19:36:25 +0000

    Update config.sh

--- KASM/config.sh
+++ KASM/config.sh
@@ -2,7 +2,7 @@
 sleep 1
 clear
 # installing and configuring elrond
-sudo ./scripts/.elrond.sh
+sudo /tmp/KASM/KASM/scripts/.elrond.sh
 updatedb
 sudo chmod -R 744 /opt/elrond/
 export PATH=$PATH:/opt/elrond
@@ -10,13 +10,13 @@ sudo chmod +x /opt/elrond/elrond.py
 sudo updatedb
 sleep 1
 # installing all other scripts
-sudo ./scripts/./code.sh
-sudo ./scripts/./virtual.sh
-sudo ./scripts/./cuckoo.sh
-sudo ./scripts/./greenbone.sh
-sudo ./scripts/./misp.sh
-sudo ./scripts/./hive.sh
-sudo ./scripts/./remnux.sh
+sudo /tmp/KASM/KASM/scripts/./code.sh
+sudo /tmp/KASM/KASM/scripts/./virtual.sh
+sudo /tmp/KASM/KASM/scripts/./cuckoo.sh
+sudo /tmp/KASM/KASM/scripts/./greenbone.sh
+sudo /tmp/KASM/KASM/scripts/./misp.sh
+sudo /tmp/KASM/KASM/scripts/./hive.sh
+sudo /tmp/KASM/KASM/scripts/./remnux.sh
 sudo apt install nmap
 gsettings get org.gnome.desktop.background picture-uri
 'file:///tmp/KASM/images/KASM.png'
