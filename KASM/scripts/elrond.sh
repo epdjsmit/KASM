9da3bb70ea894e12be89b1eb1df53d92a8d7a281
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-10-30 19:51:36 +0000

    Update elrond.sh

--- KASM/scripts/elrond.sh
+++ KASM/scripts/elrond.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-sudo ./scripts/elrond/./init.sh
-sudo ./scripts/elrond/./sift.sh
-sudo ./scripts/elrond/./apfs-fuse.sh
-sudo ./scripts/elrond/./volatility3.sh
-sudo ./scripts/elrond/./dwarf2json.sh
-sudo ./scripts/elrond/./splunk.sh
-sudo ./scripts/elrond/./elastic.sh
-sudo ./scripts/elrond/./navigator.sh
+sudo git clone https://github.com/ezaspy/elrond.git /opt/elrond
+sudo /opt/elrond/./make.sh
+cd /opt/elrond
