c9e007a70d6458649e9f17e19512b8ee4d44cb62
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-01 16:16:34 +0000

    Update config.sh

--- KASM/config.sh
+++ KASM/config.sh
@@ -15,7 +15,7 @@ read answer
 sudo /tmp/KASM/KASM/scripts/./code.sh
 sudo /tmp/KASM/KASM/scripts/./greenbone.sh
 sudo /tmp/KASM/KASM/scripts/./misp.sh
-sudo /tmp/KASM/KASM/scripts/./hive.sh
+#sudo /tmp/KASM/KASM/scripts/./hive.sh
 #sudo /tmp/KASM/KASM/scripts/./cuckoo.sh
 sudo /tmp/KASM/KASM/scripts/./remnux.sh
 gsettings get org.gnome.desktop.background picture-uri 'file:///tmp/KASM/KASM/images/background.png'
@@ -29,4 +29,5 @@ sudo reboot
 # yes '' | sudo ./vmware-install.pl -f
 # wget -O /tmp/INSTALL.sh https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh && bash /tmp/INSTALL.sh -A
     # Install the package maintainers version
+    # y
 # wget https://REMnux.org/remnux-cli && mv remnux-cli remnux && chmod +x remnux && sudo mv remnux /usr/local/bin && sudo remnux install
