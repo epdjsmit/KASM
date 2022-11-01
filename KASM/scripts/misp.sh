654f04e1541096fe856fec66e1e1b9eab35f36e9
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-01 16:16:37 +0000

    Update misp.sh

--- KASM/scripts/misp.sh
+++ KASM/scripts/misp.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 sleep 1
 wget -O /tmp/INSTALL.sh https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh
+printf "\n\n  -> When prompted, select the following options:\n      'Install the package maintainers version'\n       New MISP User: 'y'. Press ENTER to continue..."
+read answer
 bash /tmp/INSTALL.sh -A
+printf "\n\n  -> Have you made a note of the Credentials above? - MISP; DB and Local User. When ready, press ENTER to continue..."
+read answer
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
 sleep 1
