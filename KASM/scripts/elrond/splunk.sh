220accfdd22018851a48a25b1929c44bfde4d655
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-02 06:51:34 +0000

    removed elrond scripts

--- KASM/scripts/elrond/splunk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sleep 1
-clear
-printf "\n  -> Downloading Splunk...\n\n"
-sudo wget -O /opt/elrond/elrond/tools/.splunk-9.0.0.1-9e907cedecb1-linux-2.6-amd64.deb "https://download.splunk.com/products/splunk/releases/9.0.0.1/linux/splunk-9.0.0.1-9e907cedecb1-linux-2.6-amd64.deb"
-sudo updatedb
-sleep 1
