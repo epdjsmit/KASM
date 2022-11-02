34af5e81eec254b75276424aef64b67a2b524959
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-02 07:02:58 +0000

    Update init.sh

--- KASM/scripts/init.sh
+++ KASM/scripts/init.sh
@@ -2,7 +2,8 @@ mkdir /home/sansforensics/Desktop/Posters
 mv /home/sansforensics/Desktop/*.pdf /home/sansforensics/Desktop/Posters
 sudo apt update
 yes '' | sudo add-apt-repository ppa:mrazavi/gvm
-sudo apt-get install virtualbox virtualbox-ext-pack virtualbox-qt net-tools postgresql gvm nmap -y --fix-missing
+yes '' | sudo apt install postgresql gvm
+sudo apt-get install virtualbox virtualbox-ext-pack virtualbox-qt net-tools nmap -y --fix-missing
 sudo apt install systemd gnupg software-properties-common apt-transport-https wget -y --fix-missing
 sudo apt update
 sudo python3 -m pip install --upgrade pip
