620e703a903a06121ec54bcdce1560a7ca023fad
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-02 07:02:56 +0000

    Update config.sh

--- KASM/config.sh
+++ KASM/config.sh
@@ -9,6 +9,11 @@ sudo /tmp/KASM/KASM/scripts/./remnux.sh
 #sudo /tmp/KASM/KASM/scripts/./hive.sh
 sudo /tmp/KASM/KASM/scripts/./misp.sh
 sleep 1
+hostnamectl set-hostname KASM
+sudo useradd -m KASM
+sudo passwd KASM
+sleep 1
+mv /home/sansforensics/Desktop/Posters /home/KASM/Desktop/
 gsettings get org.gnome.desktop.background picture-uri 'file:///tmp/KASM/KASM/images/background.png'
 printf "\n\n  -> KASM has been successfully configured. Press ENTER to reboot..."
 read answer
