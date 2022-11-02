2f7501732b0b0649c4e4d9affddd37233bfc7e2e
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-02 06:51:38 +0000

    Update elrond.sh

--- KASM/scripts/elrond.sh
+++ KASM/scripts/elrond.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo git clone https://github.com/ezaspy/elrond.git /opt/elrond
-rm -rf /opt/elrond/elrond/config.sh
+rm -rf /opt/elrond/elrond/tools/config/scripts/finish.sh
 echo "sleep 1" > /opt/elrond/elrond/tools/config/scripts/finish.sh
 sudo /opt/elrond/./make.sh
