2d7a037232f14792d701a35f299292f868db5ee0
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-03 05:17:46 +0000

    Update config.sh

--- KASM/config.sh
+++ KASM/config.sh
@@ -21,3 +21,20 @@ sleep 1 && clear && sleep 1
 sudo reboot
 
 # yes '' | sudo ./vmware-install.pl -f
+
+#gconftool-2 --set "/apps/gnome-terminal/profiles/Default/foreground_color" --type string "#EEEEEEEEECEC"
+#gconftool-2 --set "/apps/gnome-terminal/profiles/Default/background_color" --type string "#000000000000"
+#OR
+#sudo curl -Lo /usr/bin/theme.sh 'https://git.io/JM70M' && sudo chmod +x /usr/bin/theme.sh
+#if command -v theme.sh > /dev/null; then
+#	[ -e ~/.theme_history ] && theme.sh "$(theme.sh -l|tail -n1)"
+#	# Optional
+#	#Binds C-o to the previously active theme.
+#	bind -x '"\C-o":"theme.sh $(theme.sh -l|tail -n2|head -n1)"'
+#	alias th='theme.sh -i'
+#	# Interactively load a light theme
+
+#	alias thl='theme.sh --light -i'
+#	# Interactively load a dark theme
+#	alias thd='theme.sh --dark -i'
+#fi
