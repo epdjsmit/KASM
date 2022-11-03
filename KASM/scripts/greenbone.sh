f5ecf3a6053746cac2532f3a94a4ee24a3d5616f
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-03 13:17:53 +0000

    Update greenbone.sh

--- KASM/scripts/greenbone.sh
+++ KASM/scripts/greenbone.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-export PATH=$PATH:/usr/local/sbin && export INSTALL_PREFIX=/usr/local && export SOURCE_DIR=$HOME/source && mkdir -p $SOURCE_DIR && export BUILD_DIR=$HOME/build && mkdir -p $BUILD_DIR && export INSTALL_DIR=$HOME/install && mkdir -p $INSTALL_DIR
-sudo apt update
-sudo apt install --no-install-recommends --assume-yes build-essential curl cmake pkg-config python3 python3-pip gnupg
-curl -f -L https://www.greenbone.net/GBCommunitySigningKey.asc -o /tmp/GBCommunitySigningKey.asc
-gpg --import /tmp/GBCommunitySigningKey.asc
-echo "8AE4BE429B60A59B311C2E739823FAA60ED1E580:6:" > /tmp/ownertrust.txt
-gpg --import-ownertrust < /tmp/ownertrust.txt
-export GVM_VERSION=22.4.0
+# initial config
+export PATH=$PATH:/usr/local/sbin && export INSTALL_PREFIX=/usr/local && export SOURCE_DIR=/home/gvm/source && sudo mkdir -p $SOURCE_DIR && export BUILD_DIR=/home/gvm/build && sudo mkdir -p $BUILD_DIR && export INSTALL_DIR=/home/gvm/install && sudo mkdir -p $INSTALL_DIR && sudo apt update && sudo apt install --no-install-recommends --assume-yes build-essential curl cmake pkg-config python3 python3-pip gnupg && sudo curl -f -L https://www.greenbone.net/GBCommunitySigningKey.asc -o /tmp/GBCommunitySigningKey.asc && gpg --import /tmp/GBCommunitySigningKey.asc && echo "8AE4BE429B60A59B311C2E739823FAA60ED1E580:6:" > /tmp/ownertrust.txt && gpg --import-ownertrust < /tmp/ownertrust.txt && sleep 1 && export GVM_VERSION=22.4.0 && sleep 1
 #gvm-libs
-export GVM_LIBS_VERSION=$GVM_VERSION
-sudo apt install -y libglib2.0-dev libgpgme-dev libgnutls28-dev uuid-dev libssh-gcrypt-dev libhiredis-dev libxml2-dev libpcap-dev libnet1-dev libpaho-mqtt-dev libldap2-dev libradcli-dev
-curl -f -L https://github.com/greenbone/gvm-libs/archive/refs/tags/v$GVM_LIBS_VERSION.tar.gz -o $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION.tar.gz
-curl -f -L https://github.com/greenbone/gvm-libs/releases/download/v$GVM_LIBS_VERSION/gvm-libs-$GVM_LIBS_VERSION.tar.gz.asc -o $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION.tar.gz.asc
-gpg --verify $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION.tar.gz.asc $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION.tar.gz
-tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION.tar.gz
-mkdir -p $BUILD_DIR/gvm-libs && cd $BUILD_DIR/gvm-libs && cmake $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_BUILD_TYPE=Release -DSYSCONFDIR=/etc -DLOCALSTATEDIR=/var && make -j$(nproc)
-make DESTDIR=$INSTALL_DIR install && sudo cp -rv $INSTALL_DIR/* /
+export GVM_LIBS_VERSION=$GVM_VERSION && sleep 1 && sudo wget -O /tmp/KASM/KASM/software/libpaho-mqtt1.3_1.3.8-1_amd64.deb "http://ftp.de.debian.org/debian/pool/main/p/paho.mqtt.c/libpaho-mqtt1.3_1.3.8-1_amd64.deb" && sudo wget -O /tmp/KASM/KASM/software/libpaho-mqtt-dev_1.3.8-1_amd64.deb "http://ftp.de.debian.org/debian/pool/main/p/paho.mqtt.c/libpaho-mqtt-dev_1.3.8-1_amd64.deb" && sudo apt install -y libglib2.0-dev libgpgme-dev libgnutls28-dev uuid-dev libssh-gcrypt-dev libhiredis-dev libxml2-dev libpcap-dev libnet1-dev libldap2-dev libradcli-dev && sudo dpkg -i /tmp/KASM/KASM/software/libpaho-mqtt1.3_1.3.8-1_amd64.deb && sudo dpkg -i /tmp/KASM/KASM/software/libpaho-mqtt-dev_1.3.8-1_amd64.deb && sudo curl -f -L https://github.com/greenbone/gvm-libs/archive/refs/tags/v$GVM_LIBS_VERSION.tar.gz -o $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION.tar.gz && sudo curl -f -L https://github.com/greenbone/gvm-libs/releases/download/v$GVM_LIBS_VERSION/gvm-libs-$GVM_LIBS_VERSION.tar.gz.asc -o $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION.tar.gz.asc && gpg --verify $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION.tar.gz.asc $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION.tar.gz && sudo tar -C $SOURCE_DIR -xvzf $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION.tar.gz && sudo mkdir -p $BUILD_DIR/gvm-libs && cd $BUILD_DIR/gvm-libs && sudo cmake $SOURCE_DIR/gvm-libs-$GVM_LIBS_VERSION -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_BUILD_TYPE=Release -DSYSCONFDIR=/etc -DLOCALSTATEDIR=/var && sudo make -j$(nproc) && sudo make DESTDIR=$INSTALL_DIR install && sudo cp -rv $INSTALL_DIR/* /
+
 # gvmd
 export GVMD_VERSION=$GVM_VERSION
 sudo apt install -y libglib2.0-dev libgnutls28-dev libpq-dev postgresql-server-dev-13 libical-dev xsltproc rsync libbsd-dev libgpgme-dev
