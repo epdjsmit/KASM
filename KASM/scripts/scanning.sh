656ddd94f37d9e5b10367c4a36ecca11d50b6000
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-05 19:34:06 +0000

    Create scanning.sh

---
+++ KASM/scripts/scanning.sh
@@ -0,0 +1,122 @@
+printf "\n\n  -> Ready in configure greenbone scanning..." && read answer && sleep 10782
+
+# configure scanning
+sudo visudo
+
+...
+
+# allow users of the gvm group run openvas
+%gvm ALL = NOPASSWD: /usr/local/sbin/openvas
+
+# setting up postgresql
+sudo apt install -y postgresql && sudo systemctl start postgresql@13-main
+sudo -u postgres bash && createuser -DRS gvm && createdb -O gvm gvmd && exit
+sudo -u postgres bash && psql gvmd && create role dba with superuser noinherit; && grant dba to gvm; && exit && exit
+gvmd --create-user=admin --password=kasm
+# feed import owner
+gvmd --modify-setting 78eceaec-3385-11ea-b237-28d24461215b --value `gvmd --get-users --verbose | grep admin | awk '{print $2}'`
+cat << EOF > $BUILD_DIR/ospd-openvas.service
+[Unit]
+Description=OSPd Wrapper for the OpenVAS Scanner (ospd-openvas)
+Documentation=man:ospd-openvas(8) man:openvas(8)
+After=network.target networking.service redis-server@openvas.service
+Wants=redis-server@openvas.service
+ConditionKernelCommandLine=!recovery
+
+[Service]
+Type=forking
+User=gvm
+Group=gvm
+RuntimeDirectory=ospd
+RuntimeDirectoryMode=2775
+PIDFile=/run/ospd/ospd-openvas.pid
+ExecStart=/usr/local/bin/ospd-openvas --unix-socket /run/ospd/ospd-openvas.sock --pid-file /run/ospd/ospd-openvas.pid --log-file /var/log/gvm/ospd-openvas.log --lock-file-dir /var/lib/openvas --socket-mode 0o770 --mqtt-broker-address localhost --mqtt-broker-port 1883 --notus-feed-dir /var/lib/notus/advisories
+SuccessExitStatus=SIGKILL
+Restart=always
+RestartSec=60
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo cp $BUILD_DIR/ospd-openvas.service /etc/systemd/system/
+cat << EOF > $BUILD_DIR/notus-scanner.service
+[Unit]
+Description=Notus Scanner
+Documentation=https://github.com/greenbone/notus-scanner
+After=mosquitto.service
+Wants=mosquitto.service
+ConditionKernelCommandLine=!recovery
+
+[Service]
+Type=forking
+User=gvm
+RuntimeDirectory=notus-scanner
+RuntimeDirectoryMode=2775
+PIDFile=/run/notus-scanner/notus-scanner.pid
+ExecStart=/usr/local/bin/notus-scanner --products-directory /var/lib/notus/products --log-file /var/log/gvm/notus-scanner.log
+SuccessExitStatus=SIGKILL
+Restart=always
+RestartSec=60
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo cp $BUILD_DIR/notus-scanner.service /etc/systemd/system/
+cat << EOF > $BUILD_DIR/gvmd.service
+[Unit]
+Description=Greenbone Vulnerability Manager daemon (gvmd)
+After=network.target networking.service postgresql.service ospd-openvas.service
+Wants=postgresql.service ospd-openvas.service
+Documentation=man:gvmd(8)
+ConditionKernelCommandLine=!recovery
+
+[Service]
+Type=forking
+User=gvm
+Group=gvm
+PIDFile=/run/gvmd/gvmd.pid
+RuntimeDirectory=gvmd
+RuntimeDirectoryMode=2775
+ExecStart=/usr/local/sbin/gvmd --osp-vt-update=/run/ospd/ospd-openvas.sock --listen-group=gvm
+Restart=always
+TimeoutStopSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo cp $BUILD_DIR/gvmd.service /etc/systemd/system/
+cat << EOF > $BUILD_DIR/gsad.service
+[Unit]
+Description=Greenbone Security Assistant daemon (gsad)
+Documentation=man:gsad(8) https://www.greenbone.net
+After=network.target gvmd.service
+Wants=gvmd.service
+
+[Service]
+Type=exec
+User=gvm
+Group=gvm
+RuntimeDirectory=gsad
+RuntimeDirectoryMode=2775
+PIDFile=/run/gsad/gsad.pid
+ExecStart=/usr/local/sbin/gsad --foreground --listen=127.0.0.1 --port=9392 --http-only
+Restart=always
+TimeoutStopSec=10
+
+[Install]
+WantedBy=multi-user.target
+Alias=greenbone-security-assistant.service
+EOF
+sudo cp $BUILD_DIR/gsad.service /etc/systemd/system/
+sudo systemctl daemon-reload && sudo systemctl enable notus-scanner && sudo systemctl enable ospd-openvas && sudo systemctl enable gvmd && sudo systemctl enable gsad
+# downloading vulnerability tests
+sudo -u gvm greenbone-nvt-sync
+sudo -u gvm greenbone-feed-sync --type SCAP
+sudo -u gvm greenbone-feed-sync --type CERT
+sudo -u gvm greenbone-feed-sync --type GVMD_DATA
+sudo systemctl start notus-scanner
+sudo systemctl start ospd-openvas
+sudo systemctl start gvmd
+sudo systemctl start gsad
+# starting vulnerability management
+xdg-open "http://127.0.0.1:9392" 2>/dev/null >/dev/null &
