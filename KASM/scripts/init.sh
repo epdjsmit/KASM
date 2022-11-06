1d140595b3f1afcebd97565405fe49343d9a4cf4
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-06 13:44:13 +0000

    Update init.sh

--- KASM/scripts/init.sh
+++ KASM/scripts/init.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-sudo curl -Lo /usr/bin/theme.sh 'https://git.io/JM70M' && sudo chmod +x /usr/bin/theme.sh && sudo echo "sudo /usr/bin/./theme.sh dot-gov" > set_theme_KASM.sh && sudo echo "gsettings get org.gnome.desktop.background picture-uri 'file:///tmp/KASM/KASM/images/background.png'" >> set_theme_KASM.sh && chmod 644 set_theme_KASM.sh && sudo mv set_theme_KASM.sh /etc/profile.d/ && sudo chown root:root /etc/profile.d/set_theme_KASM.sh
-sleep 1
-sudo /usr/bin/./theme.sh dot-gov
-sudo cp /tmp/KASM/KASM/images/background.png /usr/share/backgrounds/KASM.png
-gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/KASM.png
-mkdir /home/sansforensics/Desktop/Posters
-mv /home/sansforensics/Desktop/*.pdf /home/sansforensics/Desktop/Posters
-yes '' | sudo add-apt-repository ppa:mrazavi/gvm
-clear && sudo apt update
+sudo curl -Lo /usr/bin/theme.sh 'https://git.io/JM70M' && sudo chmod +x /usr/bin/theme.sh && sudo cp /tmp/KASM/KASM/images/background.png /usr/share/backgrounds/KASM.png && sudo echo "sudo /usr/bin/./theme.sh dot-gov" > set_theme_KASM.sh && sudo echo "gsettings get org.gnome.desktop.background picture-uri 'file:///tmp/KASM/KASM/images/background.png'" >> set_theme_KASM.sh && chmod 644 set_theme_KASM.sh && sudo mv set_theme_KASM.sh /etc/profile.d/ && sudo chown root:root /etc/profile.d/set_theme_KASM.sh && sleep 1 && sudo /usr/bin/./theme.sh dot-gov && gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/KASM.png
+mkdir /home/sansforensics/Desktop/Posters && mv /home/sansforensics/Desktop/*.pdf /home/sansforensics/Desktop/Posters
+yes '' | sudo add-apt-repository ppa:mrazavi/gvm && clear && sudo apt update
 sudo /tmp/KASM/KASM/scripts/./virtualbox.sh
 sudo apt-get install mlocate virtualbox virtualbox-ext-pack virtualbox-qt net-tools nmap -y --fix-missing
 sudo apt install systemd gnupg software-properties-common apt-transport-https wget -y --fix-missing
