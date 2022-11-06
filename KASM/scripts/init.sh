31d7643f271dbdfc05cec2827bf1c5ae292166ae
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-06 12:41:47 +0000

    Update init.sh

--- KASM/scripts/init.sh
+++ KASM/scripts/init.sh
@@ -4,9 +4,10 @@ mkdir /home/sansforensics/Desktop/Posters
 mv /home/sansforensics/Desktop/*.pdf /home/sansforensics/Desktop/Posters
 yes '' | sudo add-apt-repository ppa:mrazavi/gvm
 sudo apt update
-sudo apt-get install mlocate virtualbox virtualbox-ext-pack virtualbox-qt net-tools nmap -y --fix-missing
+sudo apt-get install mlocate virtualbox virtualbox-qt net-tools nmap -y --fix-missing
+#sudo apt-get install mlocate virtualbox virtualbox-ext-pack virtualbox-qt net-tools nmap -y --fix-missing
+# echo virtualbox-ext-pack virtualbox-ext-pack/license select true | sudo debconf-set-selections
 sudo apt install systemd gnupg software-properties-common apt-transport-https wget -y --fix-missing
-sudo apt update
 python3 -m pip install --upgrade pip
 sudo /tmp/KASM/KASM/scripts/./elrond.sh
 sleep 1 && clear
