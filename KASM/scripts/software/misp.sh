e04fb4253614bdf9ca590e22a181dceec80649e1
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-06 11:34:08 +0000

    Update misp.sh

--- KASM/scripts/software/misp.sh
+++ KASM/scripts/software/misp.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 sleep 1
 wget -O /tmp/INSTALL.sh https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh
-bash /tmp/INSTALL.sh -A
+sudo chown sansforensics:sansforensics /tmp/INSTALL.sh && sleep 1 && bash /tmp/INSTALL.sh -A
 printf "\n\n  -> Have you made a note of the MISP Credentials above? - MISP; DB and Local User. When ready, press ENTER to continue..."
 read answer
-sudo ufw allow 80/tcp
-sudo ufw allow 443/tcp
-sleep 1
+sleep 1 && sudo ufw allow 80/tcp
+sleep 1 && sudo ufw allow 443/tcp
