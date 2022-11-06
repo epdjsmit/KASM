01f15808bd2e26c3167029f72594d5e7b0188a27
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-06 13:35:00 +0000

    Update init.sh

--- KASM/scripts/init.sh
+++ KASM/scripts/init.sh
@@ -3,19 +3,13 @@ sudo curl -Lo /usr/bin/theme.sh 'https://git.io/JM70M' && sudo chmod +x /usr/bin
 sleep 1
 sudo /usr/bin/./theme.sh dot-gov
 sudo cp /tmp/KASM/KASM/images/background.png /usr/share/backgrounds/KASM.png
-
-
 gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/KASM.png
-
-
-gsettings get org.gnome.desktop.background picture-uri 'file:////tmp/KASM/KASM/images/background.png'
 mkdir /home/sansforensics/Desktop/Posters
 mv /home/sansforensics/Desktop/*.pdf /home/sansforensics/Desktop/Posters
 yes '' | sudo add-apt-repository ppa:mrazavi/gvm
-sudo apt update
-sudo apt-get install mlocate virtualbox virtualbox-qt net-tools nmap -y --fix-missing
-#sudo apt-get install mlocate virtualbox virtualbox-ext-pack virtualbox-qt net-tools nmap -y --fix-missing
-# echo virtualbox-ext-pack virtualbox-ext-pack/license select true | sudo debconf-set-selections
+clear && sudo apt update
+sudo /tmp/KASM/KASM/scripts/./virtualbox.sh
+sudo apt-get install mlocate virtualbox virtualbox-ext-pack virtualbox-qt net-tools nmap -y --fix-missing
 sudo apt install systemd gnupg software-properties-common apt-transport-https wget -y --fix-missing
 python3 -m pip install --upgrade pip
 sudo /tmp/KASM/KASM/scripts/./elrond.sh
