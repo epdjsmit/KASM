8f3c6b7c258468f129a60e32df155f521693a5c4
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-06 20:02:24 +0000

    Update finish.sh

--- KASM/scripts/finish.sh
+++ KASM/scripts/finish.sh
@@ -4,8 +4,8 @@ hostnamectl set-hostname KASM
 sudo useradd -m KASM
 sudo passwd KASM
 sleep 1
-mv /home/sansforensics/Desktop/Posters /home/KASM/Desktop/
-gsettings get org.gnome.desktop.background picture-uri 'file:///tmp/KASM/KASM/images/KASM.png'
+sudo curl -Lo /usr/bin/theme.sh 'https://git.io/JM70M' && sudo chmod +x /usr/bin/theme.sh && sudo cp /tmp/KASM/KASM/images/KASM.png /usr/share/backgrounds/KASM.png && sudo echo "sudo /usr/bin/./theme.sh dot-gov" > set_theme_KASM.sh && sudo echo "gsettings get org.gnome.desktop.background picture-uri 'file:///tmp/KASM/KASM/images/KASM.png'" >> set_theme_KASM.sh && chmod 644 set_theme_KASM.sh && sudo mv set_theme_KASM.sh /etc/profile.d/ && sudo chown root:root /etc/profile.d/set_theme_KASM.sh &&  gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'org.gnome.Nautilus.desktop', 'org.gnome.Screenshot.desktop', 'firefox.desktop', 'org.gnome.Terminal.desktop', 'bless.desktop', 'org.gnome.Logs.desktop', 'wireshark.desktop', 'gnome-control-center.desktop']" && sleep 1 && sudo /usr/bin/./theme.sh dot-gov && gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/KASM.png
+mkdir /home/sansforensics/Desktop/Posters && mv /home/sansforensics/Desktop/*.pdf /home/sansforensics/Desktop/Posters
 printf "\n\n  -> KASM has been successfully configured. Press ENTER to reboot..."
 read answer
 sleep 1 && clear && sleep 1
