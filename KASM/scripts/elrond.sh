a621193b7fb7bce2d1309816469d262ec22258e6
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-06 20:02:20 +0000

    Delete elrond.sh

--- KASM/scripts/elrond.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo git clone https://github.com/ezaspy/elrond.git /opt/elrond
-rm -rf /opt/elrond/elrond/tools/config/scripts/finish.sh
-sleep 1
-echo "sleep 1" > /opt/elrond/elrond/tools/config/scripts/finish.sh
-sudo /opt/elrond/./make.sh
