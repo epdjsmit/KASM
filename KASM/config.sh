4426579d060d8149f8a55d5598b2022045ed74eb
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-06 06:22:16 +0000

    Update config.sh

--- KASM/config.sh
+++ KASM/config.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sleep 1
 clear
-#/tmp/KASM/KASM/scripts/./init.sh
-#/tmp/KASM/KASM/scripts/./code.sh
-#/tmp/KASM/KASM/scripts/./remnux.sh
-#/tmp/KASM/KASM/scripts/./misp.sh
-##/tmp/KASM/KASM/scripts/./cuckoo.sh
-##/tmp/KASM/KASM/scripts/./hive.sh
+/tmp/KASM/KASM/scripts/./init.sh
+/tmp/KASM/KASM/scripts/./code.sh
+/tmp/KASM/KASM/scripts/./remnux.sh
+/tmp/KASM/KASM/scripts/./misp.sh
+#/tmp/KASM/KASM/scripts/./cuckoo.sh
+#/tmp/KASM/KASM/scripts/./hive.sh
