10ff22fc3181b9291698364b60fc8faf2ebd0ab2
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-06 12:54:47 +0000

    Update init.sh

--- KASM/scripts/init.sh
+++ KASM/scripts/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo curl -Lo /usr/bin/theme.sh 'https://git.io/JM70M' && sudo chmod +x /usr/bin/theme.sh && echo "sudo /usr/bin/./theme.sh dot-gov" > /etc/profile.d/set_theme_KASM.sh && echo "gsettings get org.gnome.desktop.background picture-uri 'file:///tmp/KASM/KASM/images/background.png'" > /etc/profile.d/set_theme_KASM.sh && sleep 1 && sudo /usr/bin/./theme.sh dot-gov && gsettings get org.gnome.desktop.background picture-uri 'file:///tmp/KASM/KASM/images/background.png'
+sudo curl -Lo /usr/bin/theme.sh 'https://git.io/JM70M' && sudo chmod +x /usr/bin/theme.sh && sudo echo "sudo /usr/bin/./theme.sh dot-gov" > /etc/profile.d/set_theme_KASM.sh && sudo echo "gsettings get org.gnome.desktop.background picture-uri 'file:///tmp/KASM/KASM/images/background.png'" > /etc/profile.d/set_theme_KASM.sh && sleep 1 && sudo /usr/bin/./theme.sh dot-gov && gsettings get org.gnome.desktop.background picture-uri 'file:///tmp/KASM/KASM/images/background.png'
 mkdir /home/sansforensics/Desktop/Posters
 mv /home/sansforensics/Desktop/*.pdf /home/sansforensics/Desktop/Posters
 yes '' | sudo add-apt-repository ppa:mrazavi/gvm
@@ -11,3 +11,19 @@ sudo apt install systemd gnupg software-properties-common apt-transport-https wg
 python3 -m pip install --upgrade pip
 sudo /tmp/KASM/KASM/scripts/./elrond.sh
 sleep 1 && clear
+
+
+#sudo apt install postgresql
+
+#sudo add-apt-repository ppa:mrazavi/gvm
+#sudo apt install gvm
+
+#sudo -u gvm -g gvm greenbone-nvt-sync
+#sudo -u gvm -g gvm greenbone-feed-sync --type CERT
+#sudo -u gvm -g gvm greenbone-feed-sync --type SCAP
+#sudo -u gvm -g gvm greenbone-feed-sync --type GVMD_DATA
+
+#export $(sudo cat /etc/default/gvmd-pg)
+#sudo -E -u gvm -g gvm gvmd --rebuild
+
+#http://localhost:9392
