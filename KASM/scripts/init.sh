a215cd45add9edd7ae1c29938596543aaa7a6ad1
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-06 15:56:45 +0000

    Update init.sh

--- KASM/scripts/init.sh
+++ KASM/scripts/init.sh
@@ -3,7 +3,7 @@ sudo curl -Lo /usr/bin/theme.sh 'https://git.io/JM70M' && sudo chmod +x /usr/bin
 mkdir /home/sansforensics/Desktop/Posters && mv /home/sansforensics/Desktop/*.pdf /home/sansforensics/Desktop/Posters
 yes '' | sudo add-apt-repository ppa:mrazavi/gvm && clear && sudo apt update
 /tmp/KASM/KASM/scripts/./virtualbox.sh && sleep 1
-sudo apt-get install mlocate virtualbox virtualbox-ext-pack virtualbox-qt net-tools nmap -y --fix-missing
+sudo apt-get install mlocate dconf2 virtualbox virtualbox-ext-pack virtualbox-qt net-tools nmap -y --fix-missing
 sudo apt install systemd gnupg software-properties-common apt-transport-https wget -y --fix-missing
 python3 -m pip install --upgrade pip
 sudo /tmp/KASM/KASM/scripts/./elrond.sh
