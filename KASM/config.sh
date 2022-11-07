afbc3f957f908188c68e38a66e57a21cc1e94816
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-07 13:08:29 +0000

    Update config.sh

--- KASM/config.sh
+++ KASM/config.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
 sleep 1 && clear && sleep 1
-/tmp/KASM/KASM/scripts/./init.sh
-/tmp/KASM/KASM/scripts/software/./code.sh
-/tmp/KASM/KASM/scripts/software/./remnux.sh
-/tmp/KASM/KASM/scripts/software/./misp.sh
-#/tmp/KASM/KASM/scripts/software/./cuckoo.sh
-#/tmp/KASM/KASM/scripts/software/./hive.sh
-#/tmp/KASM/KASM/scripts/./finish.sh
+sudo useradd -m -u 5276 -p $(openssl passwd -1 kasm) ninja && sudo usermod -aG sudo ninja
+sudo cat << EOF > custom.conf
+# GDM configuration storage
+#
+# See /usr/share/gdm/gdm.schemas for a list of available options.
+
+[daemon]
+# Uncomment the line below to force the login screen to use Xorg
+#WaylandEnable=false
+
+# Enabling automatic login
+AutomaticLogonEnable = true
+AutomaticLogin = ninja
+
+# Enabling time login
+#  TimedLoginEnable = true
+#  TimedLogin = user1
+#  TimedLoginDelay = 10
+
+[security]
+
+[xdmcp]
+
+[chooser]
+
+[debug]
+# Uncomment the line below to turn on debugging
+# More verbose logs
+# Additionally lets the X server dump core if it crashes
+#Enable=true
+EOF
+sudo rm -rf /etc/gdm3/custom.conf
+sudo chmod 644 custom.conf && sudo mv custom.conf /etc/gdm3/custom.conf && sudo chown root:root /etc/gdm3/custom.conf
+sudo -H -u ninja bash -c 'bash /tmp/KASM/KASM/scripts/./init.sh'
