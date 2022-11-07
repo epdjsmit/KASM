c48d873148e5a7edb2f8f978c788372e1229144f
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-07 20:21:08 +0000

    Update make.sh

--- make.sh
+++ make.sh
@@ -3,4 +3,4 @@ sudo chmod +x /tmp/KASM/KASM/config.sh
 sudo chmod +x /tmp/KASM/KASM/scripts/*.sh
 sudo chmod +x /tmp/KASM/KASM/scripts/software/*.sh
 sudo chmod +x /tmp/KASM/KASM/scripts/software/greenbone/*.sh
-/tmp/KASM/KASM/scripts/./user.sh
+/tmp/KASM/KASM/scripts/./temp.sh
