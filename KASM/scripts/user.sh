47ff9a32016162261b803b5701c488f2be65f4f7
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-07 20:20:09 +0000

    Delete user.sh

--- KASM/scripts/user.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo hostnamectl set-hostname kasm && sudo useradd -s /bin/bash -d /home/temp/ -m -G sudo temp && sudo passwd temp && sudo reboot
-# sudo usermod -l ninja sansforensics && sleep 1 && sudo chfn -f "ninja" ninja && sudo reboot
