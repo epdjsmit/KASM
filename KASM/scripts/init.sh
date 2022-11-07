1da10f1c693ab12b7fc393630f35b20330a8925b
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-07 10:37:24 +0000

    Update init.sh

--- KASM/scripts/init.sh
+++ KASM/scripts/init.sh
@@ -8,28 +8,3 @@ sudo git clone https://github.com/ezaspy/elrond.git /opt/elrond
 rm -rf /opt/elrond/elrond/tools/config/scripts/finish.sh && sleep 1
 echo "sleep 1" > /opt/elrond/elrond/tools/config/scripts/finish.sh
 sudo /opt/elrond/./make.sh
-
-
-#  - Configuring virtualbox-ext-pack<br>
-#    - **TAB**<br>
-#    - **&lt;Ok&gt;**<br>
-#    - Do you accept the terms of the VirtualBox PUEL license?<br>
-#      - **TAB**<br>
-#      - **&lt;Yes&gt;**<br><br>
-
-#sudo apt install postgresql
-
-#sudo add-apt-repository ppa:mrazavi/gvm
-#sudo apt install gvm
-
-#sudo -u gvm -g gvm greenbone-nvt-sync
-#sudo -u gvm -g gvm greenbone-feed-sync --type CERT
-#sudo -u gvm -g gvm greenbone-feed-sync --type SCAP
-#sudo -u gvm -g gvm greenbone-feed-sync --type GVMD_DATA
-
-#export $(sudo cat /etc/default/gvmd-pg)
-#sudo -E -u gvm -g gvm gvmd --rebuild
-
-#http://localhost:9392
-
-# gsettings get org.gnome.shell favorite-apps
