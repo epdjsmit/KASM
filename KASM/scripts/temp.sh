7b430da2c4fb5dfdb6b13d29727a4a14f90d3ebf
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-07 20:33:52 +0000

    Update temp.sh

--- KASM/scripts/temp.sh
+++ KASM/scripts/temp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo hostnamectl set-hostname kasm && sleep 1 && sudo useradd -s /bin/bash -d /home/temp/ -m -G sudo temp && sleep 1 && sudo passwd temp echo 'sudo usermod -l ninja sansforensics && sudo chfn -f "ninja" ninja && sudo passwd ninja && sudo reboot' >> /home/temp/user.sh && sleep 1 && sudo chmod +x /home/temp/user.sh && sleep 1 && sudo reboot
+sudo hostnamectl set-hostname kasm && sleep 1 && sudo useradd -s /bin/bash -d /home/temp/ -m -G sudo temp && sleep 1 && sudo passwd temp && echo 'sudo usermod -l ninja sansforensics && sudo chfn -f "ninja" ninja && sudo passwd ninja && sudo reboot' >> /home/temp/user.sh && sleep 1 && sudo chmod +x /home/temp/user.sh && sleep 1 && sudo reboot
