009876f0bf305f507cb17ed655a28154d15de29f
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-07 10:37:27 +0000

    Update finish.sh

--- KASM/scripts/finish.sh
+++ KASM/scripts/finish.sh
@@ -8,7 +8,42 @@ read answer
 sleep 1 && clear && sleep 1
 sudo reboot
 
+
+#usermod -l new_username old_username
+#groupmod -n new_groupname old_groupname
+#usermod -d /home/new_username -m new_username
+#usermod -c "New Name" new_username
+
+
+
+
+
 #yes '' | sudo ./vmware-install.pl -f
+
+#  - Configuring virtualbox-ext-pack<br>
+#    - **TAB**<br>
+#    - **&lt;Ok&gt;**<br>
+#    - Do you accept the terms of the VirtualBox PUEL license?<br>
+#      - **TAB**<br>
+#      - **&lt;Yes&gt;**<br><br>
+
+#sudo apt install postgresql
+
+#sudo add-apt-repository ppa:mrazavi/gvm
+#sudo apt install gvm
+
+#sudo -u gvm -g gvm greenbone-nvt-sync
+#sudo -u gvm -g gvm greenbone-feed-sync --type CERT
+#sudo -u gvm -g gvm greenbone-feed-sync --type SCAP
+#sudo -u gvm -g gvm greenbone-feed-sync --type GVMD_DATA
+
+#export $(sudo cat /etc/default/gvmd-pg)
+#sudo -E -u gvm -g gvm gvmd --rebuild
+
+#http://localhost:9392
+
+# gsettings get org.gnome.shell favorite-apps
+
 
 #gconftool-2 --set "/apps/gnome-terminal/profiles/Default/foreground_color" --type string "#EEEEEEEEECEC"
 #gconftool-2 --set "/apps/gnome-terminal/profiles/Default/background_color" --type string "#000000000000"
@@ -16,6 +51,5 @@ sudo reboot
 #sudo curl -Lo /usr/bin/theme.sh 'https://git.io/JM70M' && sudo chmod +x /usr/bin/theme.sh && echo "sudo /usr/bin/./theme.sh dot-gov" > /etc/profile.d/set_theme_KASM.sh && echo "gsettings get org.gnome.desktop.background picture-uri 'file:///tmp/KASM/KASM/images/background.png'" > /etc/profile.d/set_theme_KASM.sh
 
 #hostnamectl set-hostname KASM
-#sudo useradd -m KASM
-#sudo passwd KASM
+#sudo useradd -u 5276 -p $(openssl passwd -1 kasm) kasm
 #sleep 1
