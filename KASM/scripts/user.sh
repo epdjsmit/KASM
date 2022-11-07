145833c12c32555b8348cf5b0bff6f1c0e91b48a
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-07 20:10:51 +0000

    Update user.sh

--- KASM/scripts/user.sh
+++ KASM/scripts/user.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo useradd -s /bin/bash -d /home/temp/ -m -G sudo temp && sudo passwd temp
-sudo reboot
+sudo hostnamectl set-hostname kasm && sudo useradd -s /bin/bash -d /home/temp/ -m -G sudo temp && sudo passwd temp && sudo reboot
+# sudo usermod -l ninja sansforensics && sleep 1 && sudo chfn -f "ninja" ninja && sudo reboot
