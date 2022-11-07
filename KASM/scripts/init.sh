b8ee076ed2a9064f7937be3d5e4c4ecf2a8e059c
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-07 13:08:37 +0000

    Update init.sh

--- KASM/scripts/init.sh
+++ KASM/scripts/init.sh
@@ -8,3 +8,9 @@ sudo git clone https://github.com/ezaspy/elrond.git /opt/elrond
 rm -rf /opt/elrond/elrond/tools/config/scripts/finish.sh && sleep 1
 echo "sleep 1" > /opt/elrond/elrond/tools/config/scripts/finish.sh
 sudo /opt/elrond/./make.sh
+/tmp/KASM/KASM/scripts/software/./code.sh
+/tmp/KASM/KASM/scripts/software/./remnux.sh
+/tmp/KASM/KASM/scripts/software/./misp.sh
+#/tmp/KASM/KASM/scripts/software/./cuckoo.sh
+#/tmp/KASM/KASM/scripts/software/./hive.sh
+#/tmp/KASM/KASM/scripts/./finish.sh
