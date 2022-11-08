d78ba07c98d83920f4eb82c4dcc16d9287d1cb43
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-08 09:24:25 +0000

    Update temp.sh

--- KASM/scripts/temp.sh
+++ KASM/scripts/temp.sh
@@ -2,7 +2,7 @@
 sudo hostnamectl set-hostname kasm
 clear
 sleep 1
-printf "\n  -> Please enter a password for the 'temp' account...\n"
+printf "\n  -> When prompted, please enter a password for the 'temp' account...\n"
 sudo useradd -s /bin/bash -d /home/temp/ -m -G sudo temp
 sleep 1
 sudo passwd temp
@@ -13,7 +13,7 @@ sudo groupmod --new-name ninja sansforensics # change group
 sudo rm /home/sansforensics/.local/share/keyrings/login.keyring
 #sudo find /home/ninja -type f -exec egrep -H '/home/sansforensics' {} \;
 #sudo find /home/ninja -type f -exec egrep -H '/home/sansforensics' {} \; | xargs sed -i 's%/home/sansforensics%/home/ninja/‌​;g'
-echo Please enter a password for the 'ninja' account...
+echo \n  -> When prompted, please enter a password for the 'ninja' account...\n
 sudo passwd ninja # change password
 gnome-session-quit --no-prompt" >> /home/temp/user.sh
 sleep 1
