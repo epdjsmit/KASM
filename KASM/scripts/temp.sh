273a129098900bb7a4d341916f2c85612ed42e19
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-08 09:15:45 +0000

    Update temp.sh

--- KASM/scripts/temp.sh
+++ KASM/scripts/temp.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-sudo hostnamectl set-hostname kasm && sleep 1 && printf "\n\n  -> Please enter a password for the 'temp' account...\n" && sudo useradd -s /bin/bash -d /home/temp/ -m -G sudo temp && sleep 1 && sudo passwd temp && echo "echo Please enter a password for the 'ninja' account... && sudo usermod -l ninja sansforensics && sudo chfn -f 'ninja' ninja && sudo passwd ninja && sudo groupmod --new-name ninja sansforensics && sudo rm /home/sansforensics/.local/share/keyrings/login.keyring && gnome-session-quit --no-prompt" >> /home/temp/user.sh && sleep 1 && sudo chmod +x /home/temp/user.sh && sleep 1 && sudo reboot
+sudo hostnamectl set-hostname kasm
+clear
+sleep 1
+printf "\n  -> Please enter a password for the 'temp' account...\n"
+sudo useradd -s /bin/bash -d /home/temp/ -m -G sudo temp
+sleep 1
+sudo passwd temp
+echo "sudo usermod -l ninja sansforensics # change username
+sudo usermod -d /home/ninja -m ninja # change home folder
+sudo chfn -f 'ninja' ninja # change 'name'
+sudo groupmod --new-name ninja sansforensics # change group
+sudo rm /home/sansforensics/.local/share/keyrings/login.keyring
+#sudo find /home/ninja -type f -exec egrep -H '/home/sansforensics' {} \;
+#sudo find /home/ninja -type f -exec egrep -H '/home/sansforensics' {} \; | xargs sed -i 's%/home/sansforensics%/home/ninja/‌​;g'
+echo Please enter a password for the 'ninja' account...
+sudo passwd ninja # change password
+gnome-session-quit --no-prompt" >> /home/temp/user.sh
+sleep 1
+sudo chmod +x /home/temp/user.sh
+sleep 1
+sudo reboot
