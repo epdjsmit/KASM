23e9db9563e0782e5ca0551dc689355c56f71cb0
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-08 12:36:28 +0000

    Update KASM.sh

--- KASM/scripts/KASM.sh
+++ KASM/scripts/KASM.sh
@@ -3,8 +3,9 @@ sleep 1
 sudo curl -Lo /usr/bin/theme.sh 'https://git.io/JM70M'
 sudo chmod +x /usr/bin/theme.sh
 sudo cp /opt/KASM/KASM/images/KASM.png /usr/share/backgrounds/KASM.png
-sudo echo "sudo /usr/bin/./theme.sh dot-gov" > KASM_Theme.sh
-sudo echo "gsettings get org.gnome.desktop.background picture-uri 'file:///tmp/KASM/KASM/images/KASM.png'" >> KASM_Theme.sh
+sudo echo "gsettings get org.gnome.desktop.background picture-uri 'file:///tmp/KASM/KASM/images/KASM.png'
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+/usr/bin/./theme.sh dot-gov" >> KASM_Theme.sh
 chmod 755 KASM_Theme.sh
 sudo mv KASM_Theme.sh /etc/profile.d/
 sudo chown ninja:ninja /etc/profile.d/KASM_Theme.sh
@@ -18,53 +19,3 @@ printf "\n\n\n\n\n\n  -> KASM has been successfully configured. Press ENTER to r
 read answer
 sleep 1
 sudo reboot
-
-
-#usermod -l new_username old_username
-#groupmod -n new_groupname old_groupname
-#usermod -d /home/new_username -m new_username
-#usermod -c "New Name" new_username
-
-
-
-
-
-#yes '' | sudo ./vmware-install.pl -f
-
-#  - Configuring virtualbox-ext-pack<br>
-#    - **TAB**<br>
-#    - **&lt;Ok&gt;**<br>
-#    - Do you accept the terms of the VirtualBox PUEL license?<br>
-#      - **TAB**<br>
-#      - **&lt;Yes&gt;**<br><br>
-
-#sudo apt install postgresql
-
-#sudo add-apt-repository ppa:mrazavi/gvm
-#sudo apt install gvm
-
-#sudo -u gvm -g gvm greenbone-nvt-sync
-#sudo -u gvm -g gvm greenbone-feed-sync --type CERT
-#sudo -u gvm -g gvm greenbone-feed-sync --type SCAP
-#sudo -u gvm -g gvm greenbone-feed-sync --type GVMD_DATA
-
-#export $(sudo cat /etc/default/gvmd-pg)
-#sudo -E -u gvm -g gvm gvmd --rebuild
-
-#http://localhost:9392
-
-# gsettings get org.gnome.shell favorite-apps
-
-
-#gconftool-2 --set "/apps/gnome-terminal/profiles/Default/foreground_color" --type string "#EEEEEEEEECEC"
-#gconftool-2 --set "/apps/gnome-terminal/profiles/Default/background_color" --type string "#000000000000"
-#OR
-#sudo curl -Lo /usr/bin/theme.sh 'https://git.io/JM70M'
-#sudo chmod +x /usr/bin/theme.sh
-#echo "sudo /usr/bin/./theme.sh dot-gov" > /etc/profile.d/KASM_Theme.sh
-#echo "gsettings get org.gnome.desktop.background picture-uri 'file:///tmp/KASM/KASM/images/background.png'" > /etc/profile.d/KASM_Theme.sh
-
-
-#hostnamectl set-hostname KASM
-#sudo useradd -u 5276 -p $(openssl passwd -1 kasm) kasm
-#sleep 1
