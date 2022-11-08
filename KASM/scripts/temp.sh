943bd1c78f0a77f3562943f9d4b26b95eeb2d613
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-08 10:24:54 +0000

    Update temp.sh

--- KASM/scripts/temp.sh
+++ KASM/scripts/temp.sh
@@ -10,7 +10,7 @@ sudo chfn -f 'ninja' ninja # change 'name'
 sudo groupmod --new-name ninja sansforensics # change group
 sudo rm /home/ninja/.local/share/keyrings/login.keyring
 #sudo find /home/ninja -type f -exec egrep -H '/home/sansforensics' {} \; | xargs sed -i 's%/home/sansforensics%/home/ninja/‌​;g'
-echo \n  -> When prompted, please enter a password for the 'ninja' account...\n
+echo -e \n  -> When prompted, please enter a password for the 'ninja' account...\n
 sudo passwd ninja # change password
 gnome-session-quit --no-prompt" >> /home/temp/user.sh
 sudo chmod +x /home/temp/user.sh
