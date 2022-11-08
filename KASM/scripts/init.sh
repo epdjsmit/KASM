e8fbbfd39fb80412a41531587dbd5e1fe0503924
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-08 20:00:44 +0000

    Update init.sh

--- KASM/scripts/init.sh
+++ KASM/scripts/init.sh
@@ -7,9 +7,9 @@ sudo apt install systemd gnupg software-properties-common apt-transport-https wg
 python3 -m pip install --upgrade pip
 sudo git clone https://github.com/ezaspy/gandalf.git /opt/gandalf
 sudo git clone https://github.com/ezaspy/elrond.git /opt/elrond
-rm -rf /opt/elrond/elrond/tools/config/scripts/finish.sh
+sudo rm -rf /opt/elrond/elrond/tools/config/scripts/finish.sh
 sleep 1
-echo "sleep 1" > /opt/elrond/elrond/tools/config/scripts/finish.sh
+sudo echo "sleep 1" > /opt/elrond/elrond/tools/config/scripts/finish.sh
 sudo /opt/elrond/./make.sh
 sudo updatedb
 /opt/KASM/KASM/scripts/./temp.sh
