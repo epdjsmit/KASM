2c1fed0e77f5b6de5edbf55aac795ca9c3d2153c
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-08 09:15:31 +0000

    Update init.sh

--- KASM/scripts/init.sh
+++ KASM/scripts/init.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-yes '' | sudo add-apt-repository ppa:mrazavi/gvm && clear && sudo apt update
-/tmp/KASM/KASM/scripts/./virtualbox.sh && sleep 1
+yes '' | sudo add-apt-repository ppa:mrazavi/gvm
+clear
+sudo apt update
+/tmp/KASM/KASM/scripts/./virtualbox.sh
+sleep 1
 sudo apt-get install mlocate dconf2 virtualbox virtualbox-ext-pack virtualbox-qt net-tools nmap -y --fix-missing
 sudo apt install systemd gnupg software-properties-common apt-transport-https wget -y --fix-missing
 python3 -m pip install --upgrade pip
 sudo git clone https://github.com/ezaspy/elrond.git /opt/elrond
-rm -rf /opt/elrond/elrond/tools/config/scripts/finish.sh && sleep 1
+rm -rf /opt/elrond/elrond/tools/config/scripts/finish.sh
+sleep 1
 echo "sleep 1" > /opt/elrond/elrond/tools/config/scripts/finish.sh
 sudo /opt/elrond/./make.sh
 /tmp/KASM/KASM/scripts/software/./code.sh
