fc68d9f308db7026054fea6f88a89269ca721827
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-08 11:14:41 +0000

    Update KASM.sh

--- KASM/scripts/KASM.sh
+++ KASM/scripts/KASM.sh
@@ -8,19 +8,14 @@ sudo echo "gsettings get org.gnome.desktop.background picture-uri 'file:///tmp/K
 chmod 755 KASM_Theme.sh
 sudo mv KASM_Theme.sh /etc/profile.d/
 sudo chown ninja:ninja /etc/profile.d/KASM_Theme.sh
-sleep 1
 sudo /usr/bin/./theme.sh dot-gov
 gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/KASM.png
 mkdir /home/ninja/Desktop/Posters
 mv /home/sansforensics/Desktop/*.pdf /home/kasm/Desktop/Posters
-sleep 1
-clear
-sleep 1
 gsettings set org.gnome.shell favorite-apps "$(gsettings get org.gnome.shell favorite-apps | sed s/.$//), 'org.gnome.seahorse.Application.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Screenshot.desktop', 'firefox.desktop', 'org.gnome.Terminal.desktop', 'code.desktop', 'bless.desktop', 'wireshark.desktop', 'networkminer.desktop', 'cyberchef.desktop', 'die.desktop', 'jd-gui.desktop', 'ghidra.desktop', 'cutter.desktop', 'gnome-control-center.desktop']"
+sudo updatedb
 printf "\n\n\n\n\n\n  -> KASM has been successfully configured. Press ENTER to reboot..."
 read answer
-sleep 1
-clear
 sleep 1
 sudo reboot
 
