80f2589e3efe40d689f3188ce5e1ec5f005d3d96
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-08 10:24:52 +0000

    Update init.sh

--- KASM/scripts/init.sh
+++ KASM/scripts/init.sh
@@ -2,18 +2,19 @@
 yes '' | sudo add-apt-repository ppa:mrazavi/gvm
 clear
 sudo apt update
-sudo /opt/KASM/KASM/scripts/./virtualbox.sh
+/opt/KASM/KASM/scripts/./virtualbox.sh
 sleep 1
 sudo apt-get install mlocate virtualbox virtualbox-ext-pack virtualbox-qt net-tools nmap -y --fix-missing
 sudo apt install systemd gnupg software-properties-common apt-transport-https wget -y --fix-missing
 python3 -m pip install --upgrade pip
+sudo git clone https://github.com/ezaspy/gandalf.git /opt/gandalf
 sudo git clone https://github.com/ezaspy/elrond.git /opt/elrond
 rm -rf /opt/elrond/elrond/tools/config/scripts/finish.sh
 sleep 1
 echo "sleep 1" > /opt/elrond/elrond/tools/config/scripts/finish.sh
 sudo /opt/elrond/./make.sh
-sudo /opt/KASM/KASM/scripts/software/./code.sh
-sudo /opt/KASM/KASM/scripts/software/./remnux.sh
-sudo /opt/KASM/KASM/scripts/software/./misp.sh
-#sudo /opt/KASM/KASM/scripts/software/./cuckoo.sh
-#sudo /opt/KASM/KASM/scripts/software/./hive.sh
+/opt/KASM/KASM/scripts/software/./code.sh
+/opt/KASM/KASM/scripts/software/./remnux.sh
+/opt/KASM/KASM/scripts/software/./misp.sh
+#/opt/KASM/KASM/scripts/software/./cuckoo.sh
+#/opt/KASM/KASM/scripts/software/./hive.sh
