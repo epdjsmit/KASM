4ee57b401eba7f488c6ddb092e36ed4a1c792004
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-09 07:50:28 +0000

    Update config.sh

--- KASM/config.sh
+++ KASM/config.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 sudo userdel temp && sudo rm -rf /home/temp
-/opt/KASM/KASM/scripts/software/./code.sh
-/opt/KASM/KASM/scripts/software/./remnux.sh
-/opt/KASM/KASM/scripts/software/./misp.sh
+/opt/KASM/KASM/scripts/software/./code.sh && /opt/KASM/KASM/scripts/software/./remnux.sh && /opt/KASM/KASM/scripts/software/./misp.sh && /opt/KASM/KASM/scripts/./KASM.sh
 #/opt/KASM/KASM/scripts/software/./greenbone.sh
 #/opt/KASM/KASM/scripts/software/greenbone/./greenbone.sh
 #/opt/KASM/KASM/scripts/software/./cuckoo.sh
 #/opt/KASM/KASM/scripts/software/./hive.sh
-sudo /opt/KASM/KASM/scripts/./KASM.sh
+/opt/KASM/KASM/scripts/./KASM.sh
