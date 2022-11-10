9f09d95d2b79b3c260034badd6c9a68306859851
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-10 19:09:42 +0000

    Update make.sh

--- make.sh
+++ make.sh
@@ -2,4 +2,5 @@
 sudo chmod +x /opt/KASM/KASM/config.sh
 sudo chmod +x /opt/KASM/KASM/scripts/*.sh
 sudo chmod +x /opt/KASM/KASM/scripts/tools/*.sh
+python3 -m keyring --disable
 /opt/KASM/KASM/scripts/./elrond.sh
