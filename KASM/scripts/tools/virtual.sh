53e2dbe59f5c631edaab8bd0dd34a10581bb706f
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-10 11:11:27 +0000

    Update virtual.sh

--- KASM/scripts/tools/virtual.sh
+++ KASM/scripts/tools/virtual.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-# vmware
-sudo tar -xvf /opt/elrond/elrond/tools/config/VMwareTools-10.3.23-16594550.tar.gz
-sleep 1
-cd vmware-tools-distrib
-sudo rm -rf vmware-install.pl
-sudo cp /opt/elrond/elrond/tools/config/vmware-install.pl .
-sudo chmod 755 vmware-install.pl
-yes '' | sudo ./vmware-install.pl -f
-cd ..
-sudo rm -rf vmware-tools-distrib
-# virtualbox
 echo virtualbox-ext-pack virtualbox-ext-pack/license select true | sudo debconf-set-selections
