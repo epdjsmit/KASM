76687243353224cb4316b3dd4cc474e7c8531425
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-10 19:31:04 +0000

    Update elrond.sh

--- KASM/scripts/elrond.sh
+++ KASM/scripts/elrond.sh
@@ -7,8 +7,9 @@ sudo apt install systemd gnupg software-properties-common apt-transport-https wg
 python3 -m pip install --upgrade pip
 sudo git clone https://github.com/ezaspy/gandalf.git /opt/gandalf
 sudo git clone https://github.com/ezaspy/elrond.git /opt/elrond
+sudo chmod -R 744 /opt/elrond/
 sudo rm -rf /opt/elrond/elrond/config.sh
-sudo echo '#!/bin/bash
+echo '#!/bin/bash
 sleep 1
 clear
 cd /opt/elrond/elrond
