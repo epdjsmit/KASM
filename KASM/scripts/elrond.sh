b218d256c8472ecc1fea9593be9bf7bae350cc09
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-10 16:39:14 +0000

    Update elrond.sh

--- KASM/scripts/elrond.sh
+++ KASM/scripts/elrond.sh
@@ -7,8 +7,24 @@ sudo apt install systemd gnupg software-properties-common apt-transport-https wg
 python3 -m pip install --upgrade pip
 sudo git clone https://github.com/ezaspy/gandalf.git /opt/gandalf
 sudo git clone https://github.com/ezaspy/elrond.git /opt/elrond
-sudo echo "sleep 1" > /opt/elrond/elrond/tools/config/scripts/finish.sh
+sudo rm -rf /opt/elrond/elrond/config.sh
+sudo echo '#!/bin/bash
 sleep 1
+clear
+cd /opt/elrond/elrond
+/opt/elrond/elrond/tools/config/scripts/./init.sh
+/opt/elrond/elrond/tools/config/scripts/./pwsh.sh
+/opt/elrond/elrond/tools/config/scripts/./apfs-fuse.sh
+/opt/elrond/elrond/tools/config/scripts/./volatility3.sh
+/opt/elrond/elrond/tools/config/scripts/./dwarf2json.sh
+/opt/elrond/elrond/tools/config/scripts/./splunk.sh
+/opt/elrond/elrond/tools/config/scripts/./elastic.sh
+/opt/elrond/elrond/tools/config/scripts/./navigator.sh
+#/opt/elrond/elrond/tools/config/scripts/./nsrl.sh
+sudo chmod -R 744 /opt/elrond/ && sudo chmod +x /opt/elrond/elrond/elrond.py
+echo "export PATH=$PATH:/opt/elrond" >> /home/sansforensics/.bashrc
+sudo updatedb
+sleep 1' > /opt/elrond/elrond/config.sh
 sudo /opt/elrond/./make.sh
 sudo updatedb
 /opt/KASM/KASM/scripts/./user.sh
