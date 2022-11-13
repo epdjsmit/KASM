3b154a04a24e92b93360c9bddce0be306795327b
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-13 20:01:26 +0000

    Update make.sh

--- make.sh
+++ make.sh
@@ -4,5 +4,5 @@ sudo chmod +x /opt/KASM/KASM/scripts/*.*
 sudo chmod +x /opt/KASM/KASM/scripts/build/*.*
 sudo chmod +x /opt/KASM/KASM/scripts/config/*.*
 python3 -m keyring --disable
-/opt/KASM/KASM/scripts/./build.sh
-/opt/KASM/KASM/scripts/./config.sh
+/opt/KASM/KASM/./build.sh
+/opt/KASM/KASM/./config.sh
