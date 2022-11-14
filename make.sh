44f2c0029b1d5e97c6e33c505341e2cfb6779f41
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-14 10:36:09 +0000

    Update make.sh

--- make.sh
+++ make.sh
@@ -4,4 +4,5 @@ sudo chmod +x /opt/KASM/KASM/scripts/*.*
 sudo chmod +x /opt/KASM/KASM/scripts/build/*.*
 sudo chmod +x /opt/KASM/KASM/scripts/config/*.*
 python3 -m keyring --disable
+gsettings set org.gnome.desktop.screensaver lock-enabled false
 /opt/KASM/KASM/scripts/./init.sh
