a845aa560a0cbc1ca2a79fe44f3a723bc93ad069
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-14 13:55:24 +0000

    Update build.sh

--- KASM/build.sh
+++ KASM/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo userdel temp && sudo rm -rf /home/temp
-/opt/KASM/KASM/scripts/build/./code.sh
-/opt/KASM/KASM/scripts/build/./gvm.sh
-/opt/KASM/KASM/scripts/build/./misp.sh
-/opt/KASM/KASM/scripts/build/./remnux.sh
-/opt/KASM/KASM/scripts/build/./cuckoo.sh
-/opt/KASM/KASM/scripts/build/./hive.sh
-/opt/KASM/KASM/scripts/build/./metasploit.sh
-/opt/KASM/KASM/scripts/build/./extra.sh
+#/opt/KASM/KASM/scripts/build/./code.sh
+#/opt/KASM/KASM/scripts/build/./gvm.sh
+#/opt/KASM/KASM/scripts/build/./misp.sh
+#/opt/KASM/KASM/scripts/build/./remnux.sh
+#/opt/KASM/KASM/scripts/build/./cuckoo.sh
+#/opt/KASM/KASM/scripts/build/./hive.sh
+#/opt/KASM/KASM/scripts/build/./metasploit.sh
+#/opt/KASM/KASM/scripts/build/./extra.sh
 /opt/KASM/KASM/scripts/./bashrc.sh
