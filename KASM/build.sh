afdab8214742a8adcdafc022962c21d8b7e250c2
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-15 10:11:14 +0000

    Update build.sh

--- KASM/build.sh
+++ KASM/build.sh
@@ -4,8 +4,8 @@ sudo apt install code
 #/opt/KASM/KASM/scripts/build/./gvm.sh
 #/opt/KASM/KASM/scripts/build/./misp.sh
 #/opt/KASM/KASM/scripts/build/./remnux.sh
-#/opt/KASM/KASM/scripts/build/./cuckoo.sh
-#/opt/KASM/KASM/scripts/build/./hive.sh
-#/opt/KASM/KASM/scripts/build/./metasploit.sh
+#/opt/KASM/KASM/scripts/build/./cuckoo.sh # dev
+#/opt/KASM/KASM/scripts/build/./hive.sh # dev
+#/opt/KASM/KASM/scripts/build/./metasploit.sh # dev
 #/opt/KASM/KASM/scripts/build/./tools.sh
 /opt/KASM/KASM/scripts/./bashrc.sh
