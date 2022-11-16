125c1cb010407c9315b447e7b9867b13385eabbc
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-16 14:23:47 +0000

    temp kasm1

--- KASM/scripts/build/remnux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sleep 1
-wget https://REMnux.org/remnux-cli
-mv remnux-cli remnux
-chmod +x remnux
-sudo mv remnux /usr/local/bin
-sudo remnux install
-sleep 1
--- KASM/scripts/build/virtual.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo virtualbox-ext-pack virtualbox-ext-pack/license select true | sudo debconf-set-selections
-echo ""
