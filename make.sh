2a82eef6469a6cd1197499c3851eb96de52869c6
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-16 14:06:50 +0000

    Update make.sh

--- make.sh
+++ make.sh
@@ -2,5 +2,6 @@
 sudo chmod +x /opt/KASM/KASM/*.sh
 sudo chmod +x /opt/KASM/KASM/scripts/*.*
 sudo chmod +x /opt/KASM/KASM/scripts/build/*.*
+sudo chmod +x /opt/KASM/KASM/scripts/build/thehive/*.*
 sudo chmod +x /opt/KASM/KASM/scripts/config/*.*
 /opt/KASM/KASM/scripts/./init.sh
