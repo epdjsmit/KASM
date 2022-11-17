e5e5d0c0d2388e9fc18cde2de6420258087b5469
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-17 12:57:07 +0000

    Update CobaltStrike-Defence.py

--- kasm/build/config/CobaltStrike-Defence.py
+++ kasm/build/config/CobaltStrike-Defence.py
@@ -4,9 +4,13 @@ import subprocess
 def main():
     with open("/home/ninja/Desktop/CobaltStrike-Defence/README.md") as readme:
         for eachline in readme:
-            if "https://github.com/" in eachline:
+            slashes = eachline.count('/')
+            if slashes == 4 and "https://github.com/" in eachline:
                 repo = eachline.strip().split("/")[-1]
                 subprocess.Popen(["sudo", "git", "clone", eachline.strip(), "/home/ninja/Desktop/CobaltStrike-Defence/content/{}".format(repo)])
+            elif "http":
+                with open("/home/ninja/Desktop/CobaltStrike-Defence-Resources.txt", "w") as resources:
+                    resources.write(eachline)
 
 if __name__ == "__main__":
     main()
