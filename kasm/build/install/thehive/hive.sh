825f412e101a57d89c46f6b119f3352ae7a76f28
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-18 21:48:18 +0000

    Update hive.sh

--- kasm/build/install/thehive/hive.sh
+++ kasm/build/install/thehive/hive.sh
@@ -21,43 +21,7 @@ sudo touch /etc/thehive/secret.conf
 sudo chgrp thehive /etc/thehive/*.conf
 sudo chmod 640 /etc/thehive/*.conf
 cd /tmp
-echo '[Unit]
-Description=TheHive
-Documentation=https://thehive-project.org
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-WorkingDirectory=/opt/thehive
-
-User=thehive
-Group=thehive
-
-ExecStart=/opt/thehive/bin/thehive \
-	-Dconfig.file=/etc/thehive/application.conf \
-	-Dlogger.file=/etc/thehive/logback.xml \
-	-Dpidfile.path=/dev/null
-
-StandardOutput=null
-StandardError=null
-
-# Specifies the maximum file descriptor number that can be opened by this process
-LimitNOFILE=65536
-
-# Disable timeout logic and wait until process is stopped
-TimeoutStopSec=0
-
-# SIGTERM signal is used to stop the Java process
-KillSignal=SIGTERM
-
-# Java process is never killed
-SendSIGKILL=no
-
-# When a JVM receives a SIGTERM signal it exits with code 143
-SuccessExitStatus=143
-
-[Install]
-WantedBy=multi-user.target' > thehive.service
+wget https://github.com/TheHive-Project/TheHive/blob/master/package/thehive.service
 sudo cp thehive.service /etc/systemd/system/thehive.service
 sudo echo '# Service configuration
 application.baseUrl = "http://127.0.0.1:9000"
@@ -90,7 +54,7 @@ index.search {
 # The path can be updated and should belong to the user/group running thehive service. (by default: thehive:thehive)
 storage {
 provider = localfs
-localfs.location = /opt/thehive/files
+localfs.location = /opt/thp/thehive/files
 }
 
 # Additional modules
