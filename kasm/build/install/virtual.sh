2ee7abb7f46dbade7c3a134af0444b9a5084c69e
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-21 19:15:35 +0000

    Update virtual.sh

--- kasm/build/install/virtual.sh
+++ kasm/build/install/virtual.sh
@@ -5,6 +5,6 @@ sudo chmod +x VMware-Player-Full-16.2.4-20089737.x86_64.bundle
 sudo ./VMware-Player-Full-16.2.4-20089737.x86_64.bundle
 # virtualbox
 sudo wget -O virtualbox-7.0_7.0.2-154219~Ubuntu~focal_amd64.deb https://download.virtualbox.org/virtualbox/7.0.2/virtualbox-7.0_7.0.2-154219~Ubuntu~focal_amd64.deb
-sudo dpkg -i sudo wget -O virtualbox-7.0_7.0.2-154219~Ubuntu~focal_amd64.deb
+sudo dpkg -i virtualbox-7.0_7.0.2-154219~Ubuntu~focal_amd64.deb
 echo virtualbox-ext-pack virtualbox-ext-pack/license select true | sudo debconf-set-selections
 echo ""
