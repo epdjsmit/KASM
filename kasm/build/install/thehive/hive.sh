2da3407d8619307e21f901907f1bc8713170a092
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-21 06:10:29 +0000

    thehive

--- kasm/build/install/thehive/hive.sh
+++ kasm/build/install/thehive/hive.sh
@@ -6,7 +6,7 @@ sudo python3 /opt/kasm/kasm/build/install/thehive/cassandra.py
 sudo systemctl start cassandra
 sudo systemctl stop cassandra
 sudo rm -rf /var/lib/cassandra/commitlog
-sudo systemctl restart cassandra
+sudo systemctl start cassandra
 # elastic
 sudo chmod 777 /etc/elasticsearch/elasticsearch.yml
 sudo python3 /opt/kasm/kasm/build/install/thehive/elastic.py
@@ -17,130 +17,52 @@ sudo chmod 777 /etc/elasticsearch/jvm.options.d/jvm.options
 sudo python3 /opt/kasm/kasm/build/install/thehive/jvm.py
 sudo chmod 660 /etc/elasticsearch/jvm.options.d/jvm.options
 sudo chown root:elasticsearch /etc/elasticsearch/jvm.options.d/jvm.options
+sudo systemctl start elasticsearch
 # thehive
 sudo mkdir -p /opt/thehive/files
+sudo mkdir -p /opt/thehive/logs
+# 1
 cd /opt
 wget https://archives.strangebee.com/zip/thehive-latest.zip
 unzip thehive-latest.zip
-#sudo ln -s thehive-5.0.19-1 thehive
-sudo mv /opt/thehive-5.0.19-1/bin /opt/thehive
-sudo mv /opt/thehive-5.0.19-1/conf /opt/thehive
-sudo mv /opt/thehive-5.0.19-1/lib /opt/thehive
-sudo mv /opt/thehive-5.0.19-1/LICENSE /opt/thehive
-sudo rm -rf /opt/thehive-5.0.19-1
+sudo ln -s thehive-5.0.19-1 thehive
+# 2
 sudo addgroup thehive
 sudo adduser --system thehive
+sudo chown -R thehive:thehive /opt/thehive
 sudo mkdir /etc/thehive
-sudo mkdir -p /opt/thehive/logs
-# thehive.service
-echo '[Unit]
-Description=TheHive
-Documentation=https://thehive-project.org
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-WorkingDirectory=/opt/thehive
-
-User=thehive
-Group=thehive
-
-ExecStart=/opt/thehive/bin/thehive \
-	-Dconfig.file=/etc/thehive/application.conf \
-	-Dlogger.file=/etc/thehive/logback.xml \
-	-Dpidfile.path=/dev/null
-
-StandardOutput=null
-StandardError=null
-
-# Specifies the maximum file descriptor number that can be opened by this process
-LimitNOFILE=65536
-
-# Disable timeout logic and wait until process is stopped
-TimeoutStopSec=0
-
-# SIGTERM signal is used to stop the Java process
-KillSignal=SIGTERM
-
-# Java process is never killed
-SendSIGKILL=no
-
-# When a JVM receives a SIGTERM signal it exits with code 143
-SuccessExitStatus=143
-
-[Install]
-WantedBy=multi-user.target
-' > thehive.service
-sudo cp thehive.service /etc/systemd/system/thehive.service
-# application.conf
 sudo touch /etc/thehive/application.conf
-sudo echo '# Service configuration
-application.baseUrl = "http://127.0.0.1:9000"
-play.http.context = "/"
-
-# Database and index configuration
-# By default, TheHive is configured to connect to local Cassandra 4.x and a
-# local Elasticsearch services without authentication.
-db.janusgraph {
-storage {
-    backend = cql
-    hostname = ["127.0.0.1"]
-    # Cassandra authentication (if configured)
-    # username = "thehive"
-    # password = "password"
-    cql {
-    cluster-name = thp
-    keyspace = thehive
-    }
-}
-index.search {
-    backend = elasticsearch
-    hostname = ["127.0.0.1"]
-    index-name = thehive
-}
-}
-
-# Attachment storage configuration
-# By default, TheHive is configured to store files locally in the folder.
-# The path can be updated and should belong to the user/group running thehive service. (by default: thehive:thehive)
-storage {
-provider = localfs
-localfs.location = /opt/thehive/files
-}
-
-# Additional modules
-#
-# TheHive is strongly integrated with Cortex and MISP.
-# Both modules are enabled by default. If not used, each one can be disabled by
-# ommenting the configuration line.
-scalligraph.modules += org.thp.thehive.connector.cortex.CortexModule
-scalligraph.modules += org.thp.thehive.connector.misp.MispModule
-' > /etc/thehive/application.conf
+sudo chown -R root:thehive /etc/thehive
+sudo chgrp thehive /etc/thehive/application.conf
+sudo chmod 640 /etc/thehive/application.conf
 # secret.conf
 sudo touch /etc/thehive/secret.conf
 sudo chmod 777 /etc/thehive/secret.conf
 cat > /etc/thehive/secret.conf << _EOF_
 play.http.secret.key="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)"
 _EOF_
+# configuration
+sudo chmod +x /opt/kasm/kasm/build/install/thehive/service.sh
+/opt/kasm/kasm/build/install/thehive/./service.sh
+sudo chmod +x /opt/kasm/kasm/build/install/thehive/application.sh
+/opt/kasm/kasm/build/install/thehive/./application.sh
+# file storage
+sudo mkdir /opt/thp/thehive/files
+chown -R thehive:thehive /opt/thp/thehive/files
+# service
+sudo systemctl enable thehive
+sudo systemctl start thehive
+
 # permissions
-sudo chmod 755 /opt/thehive
-sudo chown -R thehive:thehive /opt/thehive
-sudo chmod 755 /etc/thehive
-sudo chown -R root:thehive /etc/thehive
-#sudo chgrp thehive /etc/thehive/application.conf
 #sudo chmod 640 /etc/thehive/application.conf
 #sudo chgrp thehive /etc/thehive/secret.conf
 #sudo chmod 640 /etc/thehive/secret.conf
 #sudo chmod -R 640 /opt/thehive/
 #sudo chown -R thehive:thehive /opt/thehive
 # logging
-sudo mkdir /var/log/thehive
-sudo chmod 640 /var/log/thehive/
-sudo chown -R thehive:thehive /var/log/thehive/
-cd ~
-sudo rm -rf /opt/thehive-latest.zip
-sudo rm -rf /opt/thehive.service
-# service
-sudo systemctl enable thehive
-sudo systemctl start thehive
-
+#sudo mkdir /var/log/thehive
+#sudo chmod 777 /var/log/thehive/
+#sudo chown -R thehive:thehive /var/log/thehive/
+#cd ~
+#sudo rm -rf /opt/thehive-latest.zip
+#sudo rm -rf /opt/thehive.service
