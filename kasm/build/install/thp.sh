c7a65ba5bcdfe465d352c5bdb9a66b5cdabb4229
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-21 14:07:30 +0000

    Update thp.sh

--- kasm/build/install/thp.sh
+++ kasm/build/install/thp.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-sleep 1
 # thehive
-sudo apt-get install ca-certificates curl gnupg lsb-release docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sleep 1
+sudo apt-get install ca-certificates curl gnupg lsb-release docker-ce docker-ce-cli containerd.io docker-compose-plugin dbus-user-session -y
 sudo docker run -d=true --rm -p 9000:9000 strangebee/thehive:latest
 # misp
 sleep 1
 wget -O /tmp/INSTALL.sh https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh
 sudo chown ninja:ninja /tmp/INSTALL.sh
 sleep 1
-sudo useradd -m -u 6477 -p $(openssl passwd -1 misp) misp
-sudo usermod -aG sudo misp
 sudo -H -u misp bash -c 'bash /tmp/INSTALL.sh -A'
 sleep 1
 sudo ufw allow 80/tcp > /dev/null 2>&1
