802668d0479f0b2e2c09b157adf02cb137a90297
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-21 14:54:15 +0000

    Delete accounts.sh

--- kasm/build/install/accounts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo adduser --system docker # thehive
-sudo groupadd docker # thehive
-sudo usermod -aG docker docker # thehive
-sudo useradd -m -u 6477 -p $(openssl passwd -1 misp) misp # misp
-sudo usermod -aG sudo misp # misp
