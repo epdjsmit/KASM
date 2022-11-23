c6fd64fe9acd280a6963142d9491fa8670b5e976
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-23 19:10:07 +0000

    Create misp.py

---
+++ kasm/build/install/misp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3 -tt
+import time
+
+def main():
+    with open("/tmp/INSTALL_orig.sh") as orig:
+        with open("/tmp/INSTALL.sh") as install:
+            for eachline in orig:
+                if "echo" in eachline and "Password:" in eachline and "PASSWORD_" in eachline and "##" not in eachline:
+                    print(eachline)
+                    print(eachline[0:-2])
+                    time.sleep(10)
+                    install.write(eachline)
+                else:
+                    install.write(eachline)
+
+if __name__ == "__main__":
+    main()
