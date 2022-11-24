0ab113bcbc3dd6dfbbe16abd1eb023903feea3bc
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-24 07:33:15 +0000

    Update misp.py

--- kasm/build/install/misp.py
+++ kasm/build/install/misp.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3 -tt
-import time
+import os
 
 def main():
+    if os.path.exists("/tmp/INSTALL.sh"):
+        os.remove("/tmp/INSTALL.sh")
+    if not os.path.exists("/home/misp/misp.txt"):
+        with open("/home/misp/misp.txt", "w") as misptxt:
+            misptxt.write("")
     with open("/tmp/INSTALL_orig.sh") as orig:
         with open("/tmp/INSTALL.sh", "a") as install:
             for eachline in orig:
-                if "echo" in eachline and "Password:" in eachline and "PASSWORD_" in eachline and "##" not in eachline:
-                    print(eachline)
-                    print(eachline[0:-2])
-                    time.sleep(10)
-                    install.write(eachline)
+                if "echo" in eachline and "##" not in eachline and "tee" not in eachline and ("Password:" in eachline or "User:" in eachline):
+                    install.write("{} | tee /home/misp/misp.txt\n".format(eachline[0:-1]))
                 else:
                     install.write(eachline)
 
