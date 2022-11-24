09f00f95269c3182fed12b12e714e5d5045ce80a
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-24 16:27:40 +0000

    Update tools.sh

--- kasm/build/install/tools.sh
+++ kasm/build/install/tools.sh
@@ -4,6 +4,7 @@ sleep 1
 printf "\n\n  -> Configuring additional tools...\n\n"
 sleep 1
 # GENERAL
+/opt/kasm/kasm/build/install/./virtual.sh
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb && sudo rm -rf google-chrome-stable_current_amd64.deb
 sudo snap install opera
 sudo apt install torbrowser-launcher -y
