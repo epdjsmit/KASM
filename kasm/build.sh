e6a3bc7696af42e4595351d67d57e8f26117ebe7
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-24 15:46:41 +0000

    Update build.sh

--- kasm/build.sh
+++ kasm/build.sh
@@ -2,10 +2,6 @@
 clear
 sleep 1
 sudo userdel temp && sudo rm -rf /home/temp
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb && sudo rm -rf google-chrome-stable_current_amd64.deb
-sudo snap install opera
-sudo apt install torbrowser-launcher -y
-sudo apt install code -y
 /opt/kasm/kasm/build/install/./gvm.sh
 /opt/kasm/kasm/build/install/./thp.sh
 /opt/kasm/kasm/build/install/./remnux.sh
