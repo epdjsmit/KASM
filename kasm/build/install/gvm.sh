15cdbba822b9e3ab5b9c8f974229cfec7a4b3705
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-25 14:20:48 +0000

    Update gvm.sh

--- kasm/build/install/gvm.sh
+++ kasm/build/install/gvm.sh
@@ -3,7 +3,7 @@ clear
 sleep 1
 printf "\n\n  -> Configuring Greenbone...\n\n"
 sleep 1
-sudo apt install postgresql gvm -y --fix-missing
+sudo apt install postgresql gvm libvirt-daemon -y --fix-missing
 sudo -u gvm -g gvm greenbone-nvt-sync
 sudo -u gvm -g gvm greenbone-feed-sync --type CERT
 sudo -u gvm -g gvm greenbone-feed-sync --type SCAP
