b79c534ffdfef20320369d9e4f11146a4a0a81be
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-25 18:36:27 +0000

    Update tools.sh

--- kasm/build/install/tools.sh
+++ kasm/build/install/tools.sh
@@ -42,16 +42,13 @@ sudo git clone https://github.com/ezaspy/gandalf.git /opt/gandalf
 # TZWorks
 sudo mkdir /opt/TZWorks
 sudo wget -O /opt/TZWorks/2022.10.19.win64.zip https://tzworks.com/prototypes/builds/2022.10.19.win64.zip
-sudo unzip /opt/TZWorks/2022.10.19.win64.zip -d /opt/TZWorks/2022.10.19.win64
 sudo wget -O /opt/TZWorks/2022.09.27.lin64.zip https://tzworks.com/prototypes/builds/2022.09.27.lin64.zip
+sudo unzip /opt/TZWorks/2022.10.19.win64.zip -d /opt/TZWorks/2022.10.19.win64
 sudo unzip /opt/TZWorks/2022.09.27.lin64.zip -d /opt/TZWorks/2022.09.27.lin64
 # BlueTeamPowerShell
 sudo mkdir /opt/BlueTeamPowerShell
 sudo wget -O /opt/BlueTeamPowerShell.zip https://blueteampowershell.com/SEC505-Scripts.zip
 sudo unzip -P 505 /opt/BlueTeamPowerShell.zip -d /opt/BlueTeamPowerShell
-# Registry Viewer
-sudo mkdir /opt/RegistryViewer
-sudo wget -O /opt/RegistryViewer/RegistryViewer_1.8.0.5.exe http://marketing.accessdata.com/Registry%20Viewer_1.8.0.5.exe
 # SIGMA
 sudo git clone https://github.com/SigmaHQ/sigma.git /opt/sigma
 # DeepBlueCLI
