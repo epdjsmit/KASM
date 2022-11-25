0525bb5540227dd117f4d55753f8296e43a87d6d
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-25 18:36:30 +0000

    Update build.sh

--- kasm/build.sh
+++ kasm/build.sh
@@ -3,8 +3,18 @@ clear
 sleep 1
 sudo userdel temp && sudo rm -rf /home/temp
 /opt/kasm/kasm/build/install/./gvm.sh
+sync; echo 1 > /proc/sys/vm/drop_caches
+sleep 4
 /opt/kasm/kasm/build/install/./thp.sh
+sync; echo 1 > /proc/sys/vm/drop_caches
+sleep 4
 /opt/kasm/kasm/build/install/./remnux.sh
+sync; echo 1 > /proc/sys/vm/drop_caches
+sleep 4
 /opt/kasm/kasm/build/install/./cuckoo.sh #dev
+sync; echo 1 > /proc/sys/vm/drop_caches
+sleep 4
 /opt/kasm/kasm/build/install/./tools.sh
+sync; echo 1 > /proc/sys/vm/drop_caches
+sleep 4
 /opt/kasm/kasm/build/./ninja.sh
