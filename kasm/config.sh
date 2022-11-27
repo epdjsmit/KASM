4ec0295f5b9b06bd583551879b696039ca856264
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-27 09:44:11 +0000

    Update config.sh

--- kasm/config.sh
+++ kasm/config.sh
@@ -4,6 +4,7 @@ sleep 1
 sudo dpkg -i /opt/elrond/elrond/tools/.splunk-9.0.0.1-9e907cedecb1-linux-2.6-amd64.deb
 sudo /opt/splunk/bin/./splunk start --accept-license --answer-yes --no-prompt --seed-passwd kasm
 sudo python3 /opt/kasm/kasm/build/config/elastic.py
+sudo cp /opt/elrond/elrond/rivendell/post/mitre/nav_json.py /opt/kasm/kasm/build/config/nav_json.py
 sudo python3 /opt/kasm/kasm/build/config/navigator.py
 sudo python3 /opt/kasm/kasm/build/config/CobaltStrike-Defence.py
 printf "\n\n\n\n\n\n  -> KASM has been successfully configured. Press ENTER to reboot..."
