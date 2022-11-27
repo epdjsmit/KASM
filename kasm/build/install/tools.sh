8aeeac5e74ef02069bf9f3558852af17895c33e7
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-27 11:32:59 +0000

    Update tools.sh

--- kasm/build/install/tools.sh
+++ kasm/build/install/tools.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # GENERAL
-/opt/kasm/kasm/build/install/./virtual.sh
+sudo wget https://download3.vmware.com/software/WKST-PLAYER-1624/VMware-Player-Full-16.2.4-20089737.x86_64.bundle
+sudo chmod +x VMware-Player-Full-16.2.4-20089737.x86_64.bundle
+sudo ./VMware-Player-Full-16.2.4-20089737.x86_64.bundle
 sudo rm -rf VMware-Player-Full-16.2.4-20089737.x86_64.bundle
 sudo rm -rf virtualbox-7.0_7.0.2-154219~Ubuntu~focal_amd64.deb
 clear
