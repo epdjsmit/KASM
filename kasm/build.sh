e6933a2d88a19c3f39e320ad66c561ff673a4d75
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-27 10:54:55 +0000

    Update build.sh

--- kasm/build.sh
+++ kasm/build.sh
@@ -2,22 +2,23 @@
 clear
 sleep 1
 sudo userdel temp && sudo rm -rf /home/temp
+
 /opt/kasm/kasm/build/install/./gvm.sh
-sync; echo 1 > /proc/sys/vm/drop_caches
-sleep 4
+sleep 4 && sync; echo 1 > /proc/sys/vm/drop_caches && sleep 4
+
 /opt/kasm/kasm/build/install/./thp.sh
-sync; echo 1 > /proc/sys/vm/drop_caches
-sleep 4
+sleep 4 && sync; echo 1 > /proc/sys/vm/drop_caches && sleep 4
+
 /opt/kasm/kasm/build/install/./remnux.sh
-sync; echo 1 > /proc/sys/vm/drop_caches
-sleep 4
+sleep 4 && sync; echo 1 > /proc/sys/vm/drop_caches && sleep 4
+
 /opt/kasm/kasm/build/install/./cuckoo.sh #dev
-sync; echo 1 > /proc/sys/vm/drop_caches
-sleep 4
+sleep 4 && sync; echo 1 > /proc/sys/vm/drop_caches && sleep 4
+
 /opt/kasm/kasm/build/install/./tools.sh
-sync; echo 1 > /proc/sys/vm/drop_caches
-sleep 4
+sleep 4 && sync; echo 1 > /proc/sys/vm/drop_caches && sleep 4
+
 /opt/kasm/kasm/./config.sh
-sync; echo 1 > /proc/sys/vm/drop_caches
-sleep 4
+sleep 4 && sync; echo 1 > /proc/sys/vm/drop_caches && sleep 4
+
 /opt/kasm/kasm/build/./ninja.sh
