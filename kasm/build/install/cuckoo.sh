4bd2f2a7410dd4884224c096d1a464d042a4e1e8
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-28 13:25:19 +0000

    Update cuckoo.sh

--- kasm/build/install/cuckoo.sh
+++ kasm/build/install/cuckoo.sh
@@ -4,54 +4,54 @@ clear
 sleep 1
 printf "\n\n  -> Installing Cuckoo...\n\n"
 sleep 1
-wget -O cuckoo-setup.zip https://github.com/cyberthint/cuckoo-sandbox-installation-script/archive/refs/heads/master.zip && unzip cuckoo-setup.zip && cd cuckoo-sandbox-installation-script-master && sed -i '6d' run-as-root.sh && chmod a+x run-as-root.sh
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
-python get-pip.py
-sudo apt-get update
-sudo apt install software-properties-common build-essential python python-dev libffi-dev libssl-dev python-setuptools libjpeg-dev zlib1g-dev swig libpq-dev tcpdump genisoimage apt-transport-https ca-certificates curl gnupg lsb-release docker-ce docker-ce-cli containerd.io -y
-pip2 install virtualenv
-sudo adduser --disabled-password --gecos "" cuckoo
+echo 'deb http://security.ubuntu.com/ubuntu xenial-security main' | sudo tee /etc/apt/sources.list.d/xenial-security.list
+wget -c http://archive.ubuntu.com/ubuntu/pool/main/p/python-mysqldb/python-mysqldb_1.3.10-1build1_amd64.deb
+sudo apt update
+sleep 10
+sudo apt update
+sudo apt-get install libmysqlclient20 libffi-dev libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev python-setuptools libjpeg-dev zlib1g-dev libxml2-dev libxslt1-dev libevent-dev libpcre3 libpcre3-dev libtool libpcre++-dev g++ automake dkms unzip wget python-sqlalchemy python-jinja2 python-magic python-mysqldb python-chardet mongodb postgresql libpq-dev tcpdump libcap2-bin apparmor-utils ssdeep python-pyrex subversion libfuzzy-dev swig vsftpd ./python-mysqldb_1.3.10-1build1_amd64.deb -y
+pip2 install virtualenv bson dpkt gridfs-fuse bottle pefile pillow pycrypto lxml cybox==2.0.1.4 maec "Django<2" m2crypto==0.31.0 IPython==5.0 jupyter openpyxl ujson
+sudo -H pip2 install ansible --upgrade
+sudo aa-disable /usr/sbin/tcpdump
+sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
+getcap /usr/sbin/tcpdump
+wget -O distorm-3.4.1.tar.gz https://github.com/gdabah/distorm/archive/refs/tags/v3.4.1.tar.gz
+wget -O yara-3.8.1.tar.gz https://github.com/VirusTotal/yara/archive/refs/tags/v3.8.1.tar.gz
+tar -zxvf distorm-3.4.1.tar.gz
+cd distorm-3.4.1
+sudo python2.7 setup.py install
+sudo apt-get install libjansson-dev libmagic-dev libtool-bin -y
+cd ..
+sudo tar -zxvf yara-3.8.1.tar.gz
+cd yara-3.8.1
+sudo ./bootstrap.sh
+sudo ./configure --with-crypto --enable-magic --enable-cuckoo
+sudo make
+sudo make install
+sudo -H pip2 install yara-python
+cd ..
+sudo useradd -m -u 6477 -p $(openssl passwd -1 cuckoo) cuckoo
+sudo usermod -aG sudo cuckoo
+sudo usermod -L cuckoo
+sudo usermod -a -G kvm cuckoo
+sudo groupadd libvirt
+sudo usermod -a -G libvirt cuckoo
 sudo groupadd pcap
 sudo usermod -a -G pcap cuckoo
 sudo chgrp pcap /usr/sbin/tcpdump
 sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
-sudo apt install apparmor-utils -y
-sudo aa-disable /usr/sbin/tcpdump
-sudo cp ./run-as-cuckoo.sh /home/cuckoo/run-as-cuckoo.sh
-sudo chown cuckoo:cuckoo /home/cuckoo/run-as-cuckoo.sh
-sudo chmod a+x /home/cuckoo/run-as-cuckoo.sh
-sudo cp ./services/start-cuckoo.sh /home/cuckoo/start-cuckoo.sh
-sudo chown cuckoo:cuckoo /home/cuckoo/start-cuckoo.sh
-sudo chmod a+x /home/cuckoo/start-cuckoo.sh
-sudo cp -R ./conf /home/cuckoo/conf
-sudo chown -R cuckoo:cuckoo /home/cuckoo/conf
-#wget --no-check-certificate -O win10.iso "https://onedrive.live.com/download?cid=6B2C69CA86AC3FC8&resid=6B2C69CA86AC3FC8%213079389&authkey=ACz-4e4PU0wPT5E"
-mkdir -p /mnt/cuckoo/win10
-#sudo mount -o ro,loop win10.iso /mnt/cuckoo/win10
-sudo mount -o ro,loop /mnt/hgfs/kasm/TEST/Windows10.iso /mnt/cuckoo/win10
-sudo apt install virtualbox-6.0 -y # virtualbox virtualbox-ext-pack virtualbox-qt
-sudo usermod -a -G vboxusers cuckoo
-vboxmanage hostonlyif create
-echo 1 | sudo tee -a /proc/sys/net/ipv4/ip_forward
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo cp ./docker-compose.yml /start-cuckoo-services.yml
-sudo cp ./services/cuckoo-docker.service /etc/systemd/system/cuckoo-docker.service
-sudo systemctl enable cuckoo-docker.service
-sudo cp ./services/cuckoo-settings.service /etc/systemd/system/cuckoo-settings.service
-sudo systemctl enable cuckoo-settings.service
-sudo cp ./services/cuckoo.service /etc/systemd/system/cuckoo.service
-sudo systemctl enable cuckoo.service
-sudo cp ./services/cuckoo-web.service /etc/systemd/system/cuckoo-web.service
-sudo systemctl enable cuckoo-web.service
-sudo cp ./services/cuckoo-api.service /etc/systemd/system/cuckoo-api.service
-sudo systemctl enable cuckoo-api.service
-
-#$ su - cuckoo
-#$ cd ~
-#$ ./run-as-cuckoo.sh
-# sudo -H -u cuckoo bash -c '/home/cuckoo/./run-as-cuckoo.sh'
-
-#$ source /home/cuckoo/cuckoo/bin/activate
-#$ cuckoo
+sudo mkdir -p /home/cuckoo/vmshared/pub
+sudo chown -R cuckoo:cuckoo /home/cuckoo
+sudo chmod -R ug=rwX,o=rX /home/cuckoo/vmshared/
+sudo chmod -R ugo=rwX /home/cuckoo/vmshared/pub
+sudo cat /etc/vsftpd.conf > vsftpd.conf.bak
+sudo rm -rf /etc/vsftpd.conf
+python3 /opt/kasm/kasm/build/install/cuckoo.py
+sudo cp vsftpd.conf /etc/
+sudo chmod 644 /etc/vsftpd.conf
+sudo service vsftpd restart
+sudo mv run_cuckoo.sh /home/cuckoo/
+sudo chmod +x /home/cuckoo/run_cuckoo.sh
+sudo chown cuckoo:cuckoo /home/cuckoo/run_cuckoo.sh
+sudo -H -u cuckoo bash -c './run_cuckoo.sh'
+cuckoo web runserver
