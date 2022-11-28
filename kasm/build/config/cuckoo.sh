c6f932ce26b3bf921c6314fc6e22a6bee6f5fbcb
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-28 14:52:28 +0000

    Update cuckoo.sh

--- kasm/build/config/cuckoo.sh
+++ kasm/build/config/cuckoo.sh
@@ -6,7 +6,7 @@ sleep 1
 
 sudo cat /etc/vsftpd.conf > vsftpd.conf.bak
 sudo rm -rf /etc/vsftpd.conf
-python3 /opt/kasm/kasm/build/install/cuckoo.py
+python3 /opt/kasm/kasm/build/config/cuckoo.py
 sudo cp vsftpd.conf /etc/
 sudo chmod 644 /etc/vsftpd.conf
 sudo service vsftpd restart
@@ -14,4 +14,6 @@ sudo mv run_cuckoo.sh /home/cuckoo/
 sudo chmod +x /home/cuckoo/run_cuckoo.sh
 sudo chown cuckoo:cuckoo /home/cuckoo/run_cuckoo.sh
 sudo -H -u cuckoo bash -c '/home/cuckoo/./run_cuckoo.sh'
+sudo service mongodb start
+#nano /root/.cuckoo/conf/reporting.conf
 cuckoo web runserver
