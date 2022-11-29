296e7a91eb83ed38b6d4778f0ef0fecf756aece8
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-29 10:50:24 +0000

    Update cuckoo.sh

--- kasm/build/config/cuckoo.sh
+++ kasm/build/config/cuckoo.sh
@@ -8,9 +8,10 @@ sudo virtualenv venv
 pip2 install -U setuptools cuckoo
 #/home/ninja/.local/bin/cuckoo -d
 sudo cp /opt/kasm/kasm/build/config/agent.pyw /home/cuckoo/vmshared/
-systemctl enable mongodb.service
+sudo systemctl enable mongodb.service
 sudo service mongodb start
 sudo rm -rf /home/ninja/.cuckoo/conf/reporting.conf
 sudo mv reporting.conf /home/ninja/.cuckoo/conf/reporting.conf
 sudo service mongodb restart
 /home/ninja/.local/bin/cuckoo web runserver
+deactivate
