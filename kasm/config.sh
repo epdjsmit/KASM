a097caa451d1b3f5aa06b3a393b55a22d4870fd8
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-29 14:06:22 +0000

    Update config.sh

--- kasm/config.sh
+++ kasm/config.sh
@@ -3,17 +3,21 @@ clear
 sleep 1
 sudo dpkg -i /opt/elrond/elrond/tools/.splunk-9.0.0.1-9e907cedecb1-linux-2.6-amd64.deb
 sudo /opt/splunk/bin/./splunk start --accept-license --answer-yes --no-prompt --seed-passwd kasm
-sudo python3 /opt/kasm/kasm/build/config/elastic.py
-sudo cp /opt/elrond/elrond/rivendell/post/mitre/nav_json.py /opt/kasm/kasm/build/config/nav_json.py
 sudo python3 /opt/kasm/kasm/build/config/navigator.py
 sudo /opt/kasm/kasm/build/config/./cuckoo.sh
 sudo python3 /opt/kasm/kasm/build/config/CobaltStrike-Defence.py
-# kasm applications to start upon logging in
-echo '#!/bin/bash
-sudo pg_ctlcluster 12 main start # gvm
-cuckoo web runserver # cuckoo
-' > /home/ninja/kasm.sh
-sudo chmod 755 /home/ninja/kasm.sh
+# start kasm applications - sudo /home/ninja/./kick-ass.sh
+printf "
+        I came here to drink milk and kick ass, and I've just finished my milk.
+                                                        - Word (Moss), 2010
+
+"
+echo "#!/bin/bash
+
+sudo pg_ctlcluster 12 main start
+cuckoo web runserver
+" > /home/ninja/kick-ass.sh
+sudo chmod +x /home/ninja/kick-ass.sh
 # success - final reboot
 printf "\n\n\n\n\n\n  -> KASM has been successfully configured. Press ENTER to reboot..."
 read answer
