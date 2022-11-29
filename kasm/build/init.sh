00c83cd080004cff8419de38e75d68327d9dc12c
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-29 14:06:32 +0000

    Update init.sh

--- kasm/build/init.sh
+++ kasm/build/init.sh
@@ -10,6 +10,11 @@ sudo apt-get clean
 # initialising kasm
 sleep 1
 sudo chmod -R 755 /opt/kasm
+wget -O agent.pyw "https://onedrive.live.com/embed?cid=6B2C69CA86AC3FC8&resid=6B2C69CA86AC3FC8%213083287&authkey=AO9ecFMM8pXll1E"
+sudo mv agent.pyw /opt/kasm/kasm/build/config/
+wget -O elastic.py "https://onedrive.live.com/embed?cid=6B2C69CA86AC3FC8&resid=6B2C69CA86AC3FC8%213083290&authkey=ADWrcfFoW6cbo2M"
+cp elastic.py /opt/kasm/kasm/build/config/
+sudo cp /opt/elrond/elrond/rivendell/post/mitre/nav_json.py /opt/kasm/kasm/build/config/nav_json.py
 /opt/kasm/kasm/build/install/./repos.sh
 python3 -m pip install --upgrade pip
 python2.7 -m pip install --upgrade pip
