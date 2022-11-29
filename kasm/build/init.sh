92fd19468003bbe09cd14cbfcd42f2fe56f7f979
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-29 10:02:48 +0000

    Update init.sh

--- kasm/build/init.sh
+++ kasm/build/init.sh
@@ -16,35 +16,13 @@ unzip python.zip
 mv python/*.py /opt/kasm/kasm/build/
 mv python/install/*.py /opt/kasm/kasm/build/install/
 mv python/config/*.py /opt/kasm/kasm/build/config/
-rm -rf python.zip && rm -rf python
+rm -rf python.zip
+rm -rf python
 /opt/kasm/kasm/build/install/./repos.sh
-sudo apt-get install mlocate dconf-tools net-tools nmap -y --fix-missing
-sudo apt install systemd gnupg software-properties-common apt-transport-https wget -y --fix-missing
 python3 -m pip install --upgrade pip
 python2.7 -m pip install --upgrade pip
 sudo git clone https://github.com/ezaspy/elrond.git /opt/elrond
-sudo rm -rf /opt/elrond/elrond/config.sh
-sudo echo '#!/bin/bash
-sleep 1
-clear
-cd /opt/elrond/elrond
-/opt/elrond/elrond/tools/config/scripts/./init.sh
-/opt/elrond/elrond/tools/config/scripts/./pwsh.sh
-/opt/elrond/elrond/tools/config/scripts/./apfs-fuse.sh
-/opt/elrond/elrond/tools/config/scripts/./volatility3.sh
-/opt/elrond/elrond/tools/config/scripts/./dwarf2json.sh
-/opt/elrond/elrond/tools/config/scripts/./splunk.sh
-/opt/elrond/elrond/tools/config/scripts/./elastic.sh
-/opt/elrond/elrond/tools/config/scripts/./navigator.sh
-/opt/elrond/elrond/tools/config/scripts/./nsrl.sh
-sudo chmod -R 744 /opt/elrond/ && sudo chmod +x /opt/elrond/elrond/elrond.py
-echo "export PATH=$PATH:/opt/elrond" >> /home/sansforensics/.bashrc
-sudo updatedb
-sleep 1' > config.sh
-chmod +x config.sh
-sudo mv config.sh /opt/elrond/elrond/
-sudo chmod -R 755 /opt/elrond/
-sudo chown -R root:root /opt/elrond/
+sed -i '26d' /opt/elrond/elrond/config.sh
 sudo /opt/elrond/./make.sh
 sudo updatedb
 sleep 2
