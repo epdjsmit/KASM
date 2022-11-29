b67a0dd9384433056dc345636a20d655fe01abda
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-29 20:08:42 +0000

    Update build.sh

--- kasm/build.sh
+++ kasm/build.sh
@@ -6,7 +6,7 @@ sleep 100000000
 /opt/kasm/kasm/build/install/./gvm.sh
 /opt/kasm/kasm/build/install/./thp.sh
 /opt/kasm/kasm/build/install/./remnux.sh
-/opt/kasm/kasm/build/install/./cuckoo.sh #dev
+/opt/kasm/kasm/build/install/./cuckoo.sh
 /opt/kasm/kasm/build/install/./tools.sh
 /opt/kasm/kasm/./config.sh
 /opt/kasm/kasm/build/./ninja.sh
