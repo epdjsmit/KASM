03af6f477fc08cdfa769128f496a4c256a1db6c6
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-29 12:25:36 +0000

    Update init.sh

--- kasm/build/init.sh
+++ kasm/build/init.sh
@@ -10,13 +10,6 @@ sudo apt-get clean
 # initialising kasm
 sleep 1
 sudo chmod -R 755 /opt/kasm
-wget -O python.zip "https://onedrive.live.com/download?cid=6B2C69CA86AC3FC8&resid=6B2C69CA86AC3FC8%213083090&authkey=AOMnckB0SdPmt60"
-unzip python.zip
-sudo mv python/*.py /opt/kasm/kasm/build/
-sudo mv python/install/*.py /opt/kasm/kasm/build/install/
-sudo mv python/config/*.py /opt/kasm/kasm/build/config/
-rm -rf python.zip
-rm -rf python
 /opt/kasm/kasm/build/install/./repos.sh
 python3 -m pip install --upgrade pip
 python2.7 -m pip install --upgrade pip
