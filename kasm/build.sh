8d6cea8426f78b14bad74a981ea6badcda372301
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-30 12:25:07 +0000

    Update build.sh

--- kasm/build.sh
+++ kasm/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 clear
-sleep 10000000
+sleep 10
 sudo userdel temp && sudo rm -rf /home/temp
+options=$(cat /home/ninja/.vars)
+echo $options
+sleep 100000
 /opt/kasm/kasm/build/install/./gvm.sh
 /opt/kasm/kasm/build/install/./thp.sh
 /opt/kasm/kasm/build/install/./remnux.sh
