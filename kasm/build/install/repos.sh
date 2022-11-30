fed59e101b94656a093dd5f31ddc9d812be962ec
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-30 12:33:33 +0000

    Update repos.sh

--- kasm/build/install/repos.sh
+++ kasm/build/install/repos.sh
@@ -4,8 +4,6 @@ sleep 1
 printf "\n\n  -> Configuring repositories...\n\n"
 sleep 1
 sudo apt update
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add - # vscode
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" # vscode
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add - # virtualbox
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add - # virtualbox
 sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" # virtualbox
