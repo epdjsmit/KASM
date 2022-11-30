b747d54aff366aafb2341cb8331d335b954562f9
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-11-30 18:04:21 +0000

    Update tools.sh

--- kasm/build/install/tools.sh
+++ kasm/build/install/tools.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 clear
 sleep 1
-printf "\n\n  -> Installing virtualisation software...\n\n"
-sleep 1
-sudo wget https://download3.vmware.com/software/WKST-PLAYER-1624/VMware-Player-Full-16.2.4-20089737.x86_64.bundle
-sudo chmod +x VMware-Player-Full-16.2.4-20089737.x86_64.bundle
-sudo ./VMware-Player-Full-16.2.4-20089737.x86_64.bundle
-sudo rm -rf VMware-Player-Full-16.2.4-20089737.x86_64.bundle
-echo "sudo wget -O virtualbox-7.0_7.0.2-154219~Ubuntu~focal_amd64.deb https://download.virtualbox.org/virtualbox/7.0.2/virtualbox-7.0_7.0.2-154219~Ubuntu~focal_amd64.deb
-sudo dpkg -i virtualbox-7.0_7.0.2-154219~Ubuntu~focal_amd64.deb
-echo virtualbox-ext-pack virtualbox-ext-pack/license select true | sudo debconf-set-selections" > virtualbox.sh
-chmod +x virtualbox.sh
-./virtualbox.sh
-sudo rm -rf virtualbox.sh
-clear
-sleep 1
 printf "\n\n  -> Installing browsers...\n\n"
 sleep 1
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb && sudo rm -rf google-chrome-stable_current_amd64.deb
