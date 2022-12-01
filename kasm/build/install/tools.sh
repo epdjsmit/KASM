152b8fbbf205292ee208cec4e9dd48449fc18460
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-12-01 14:09:30 +0000

    Update tools.sh

--- kasm/build/install/tools.sh
+++ kasm/build/install/tools.sh
@@ -73,3 +73,4 @@ sudo git clone https://github.com/sherlock-project/sherlock.git /opt/sherlock
 sudo git clone https://github.com/jonbakerfish/TweetScraper.git /opt/TweetScraper
 sudo git clone https://github.com/Dheerajmadhukar/karma_v2.git /opt/karma
 sudo su -c "echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a" root
+sleep 10
