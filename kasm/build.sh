b0ddaebe45aba5001f04b2c846d67b9a4dc645d3
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-12-01 16:46:10 +0000

    Update build.sh

--- kasm/build.sh
+++ kasm/build.sh
@@ -5,10 +5,12 @@ sudo userdel temp && sudo rm -rf /home/temp
 #sleep 10000000
 #options=$(cat /home/ninja/.vars)
 #echo $options
-/opt/kasm/kasm/build/install/./gvm.sh
-/opt/kasm/kasm/build/install/./thp.sh
-/opt/kasm/kasm/build/install/./remnux.sh
-/opt/kasm/kasm/build/install/./cuckoo.sh
-/opt/kasm/kasm/build/install/./tools.sh
-/opt/kasm/kasm/./config.sh
-/opt/kasm/kasm/build/./ninja.sh
+
+/opt/kasm/kasm/build/install/./thp.sh # testing
+/opt/kasm/kasm/build/install/./gvm.sh # untested
+/opt/kasm/kasm/build/install/./cuckoo.sh # untested
+/opt/kasm/kasm/build/install/./remnux.sh # untested
+/opt/kasm/kasm/build/install/./tools.sh # untested
+
+/opt/kasm/kasm/./config.sh # untested
+/opt/kasm/kasm/build/./ninja.sh # untested
