456b06bd2a4e9c0dfbd0f283d0c760dc8bc833c1
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-12-02 19:37:54 +0000

    Update gvm.sh

--- kasm/build/install/gvm.sh
+++ kasm/build/install/gvm.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 clear
 sleep 1
-printf "\n\n  -> Configuring Greenbone...\n\n"
-sleep 1
 sudo apt install postgresql gvm libvirt-daemon -y --fix-missing
 sudo -u gvm -g gvm greenbone-nvt-sync
 sudo -u gvm -g gvm greenbone-feed-sync --type CERT
@@ -11,4 +9,3 @@ sudo -u gvm -g gvm greenbone-feed-sync --type GVMD_DATA
 export $(sudo cat /etc/default/gvmd-pg)
 sudo pg_ctlcluster 12 main start
 sudo su -c "echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a" root
-sleep 10
