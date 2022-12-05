d1afbbb005836cc4705ce74bfdef6da501147183
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-12-05 16:06:10 +0000

    Update cuckoo.sh

--- kasm/build/install/cuckoo.sh
+++ kasm/build/install/cuckoo.sh
@@ -53,12 +53,12 @@ sudo chmod -R ugo=rwX /home/cuckoo/vmshared/pub
 sudo virtualenv venv
 . venv/bin/activate
 pip2 install -U setuptools cuckoo
-/home/ninja/.local/bin/cuckoo -d
+/home/sansforensics/.local/bin/cuckoo -d
 sudo cp /opt/kasm/kasm/build/install/agent.pyw /home/cuckoo/vmshared/
 sudo systemctl enable mongodb.service
 sudo service mongodb start
-sed -i '45s/enabled = no/enabled = yes/' /home/ninja/.cuckoo/conf/reporting.conf
-sed -i '10s/= no/= yes/' /home/ninja/.cuckoo/conf/cuckoo.conf
+sed -i '45s/enabled = no/enabled = yes/' /home/sansforensics/.cuckoo/conf/reporting.conf
+sed -i '10s/= no/= yes/' /home/sansforensics/.cuckoo/conf/cuckoo.conf
 sudo service mongodb restart
 deactivate
 sudo su -c "echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a" root
