19bc24d1e888d685d302c1e076e778be91ea472a
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-12-05 18:31:46 +0000

    suppress commend line output

--- kasm/build/init.sh
+++ kasm/build/init.sh
@@ -7,30 +7,30 @@ gsettings set org.gnome.desktop.lockdown disable-lock-screen true
 gsettings set org.gnome.desktop.session idle-delay 0
 
 # creating cuckoo user
-sudo useradd -m -u 6478 -p $(openssl passwd -1 cuckoo) cuckoo && sudo usermod -aG sudo cuckoo # > /dev/null 2>&1
+sudo useradd -m -u 6478 -p $(openssl passwd -1 cuckoo) cuckoo && sudo usermod -aG sudo cuckoo > /dev/null 2>&1
 printf "\n >> Please enter a password for the cuckoo account\n"
 sudo passwd cuckoo
 
 # remnux must be installed before anything else
 printf "\n >> Downloading and installing REMnux\n"
-wget https://REMnux.org/remnux-cli # > /dev/null 2>&1
-mv remnux-cli remnux # > /dev/null 2>&1
-chmod +x remnux # > /dev/null 2>&1
-sudo mv remnux /usr/local/bin # > /dev/null 2>&1
-sudo remnux install # > /dev/null 2>&1
+wget https://REMnux.org/remnux-cli > /dev/null 2>&1
+mv remnux-cli remnux
+chmod +x remnux
+sudo mv remnux /usr/local/bin
+sudo remnux install > /dev/null 2>&1
 
 # configuring repositories
 printf "\n >> Updating repositories...\n"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg # > /dev/null 2>&1 # thehive
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg > /dev/null 2>&1 # thehive
 sleep 4
-yes '' | echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null # > /dev/null 2>&1 # thehive / cuckoo
+yes '' | echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null # thehive / cuckoo
 sleep 4
-yes '' | sudo add-apt-repository ppa:mrazavi/gvm # > /dev/null 2>&1 # greenbone
-echo 'deb http://security.ubuntu.com/ubuntu xenial-security main' | sudo tee /etc/apt/sources.list.d/xenial-security.list # > /dev/null 2>&1 # cuckoo
-wget -O /tmp/python-mysqldb_1.3.10-1build1_amd64.deb http://archive.ubuntu.com/ubuntu/pool/main/p/python-mysqldb/python-mysqldb_1.3.10-1build1_amd64.deb # > /dev/null 2>&1 # cuckoo
+yes '' | sudo add-apt-repository ppa:mrazavi/gvm > /dev/null 2>&1 # greenbone
+echo 'deb http://security.ubuntu.com/ubuntu xenial-security main' | sudo tee /etc/apt/sources.list.d/xenial-security.list > /dev/null 2>&1 # cuckoo
+wget -O /tmp/python-mysqldb_1.3.10-1build1_amd64.deb http://archive.ubuntu.com/ubuntu/pool/main/p/python-mysqldb/python-mysqldb_1.3.10-1build1_amd64.deb > /dev/null 2>&1 # cuckoo
 sleep 4
-#yes '' | sudo add-apt-repository ppa:micahflee/ppa # > /dev/null 2>&1 # tor
-sudo apt update # > /dev/null 2>&1
+#yes '' | sudo add-apt-repository ppa:micahflee/ppa > /dev/null 2>&1 # tor
+sudo apt update > /dev/null 2>&1
 echo " >>  Repositories updated\n"
 
 # installing kasm software suites
@@ -38,48 +38,48 @@ echo " >>  Repositories updated\n"
 
 # preparing cuckoo
 printf "\n >>  Preparing cuckoo sandbox\n"
-wget -O agent.pyw "https://onedrive.live.com/embed?cid=6B2C69CA86AC3FC8&resid=6B2C69CA86AC3FC8%213083287&authkey=AO9ecFMM8pXll1E" # > /dev/null 2>&1
-sudo mv agent.pyw /opt/kasm/kasm/build/install/ # > /dev/null 2>&1
-/opt/kasm/kasm/build/install/./cuckoo.sh
-/opt/kasm/kasm/build/install/./thp.sh
-/opt/kasm/kasm/build/install/./tools.sh # > /dev/null 2>&1
-sudo apt install postgresql gvm libvirt-daemon -y --fix-missing
-sudo -u gvm -g gvm greenbone-nvt-sync
-sudo -u gvm -g gvm greenbone-feed-sync --type CERT
-sudo -u gvm -g gvm greenbone-feed-sync --type SCAP
-sudo -u gvm -g gvm greenbone-feed-sync --type GVMD_DATA
+wget -O agent.pyw "https://onedrive.live.com/embed?cid=6B2C69CA86AC3FC8&resid=6B2C69CA86AC3FC8%213083287&authkey=AO9ecFMM8pXll1E" > /dev/null 2>&1
+sudo mv agent.pyw /opt/kasm/kasm/build/install/ > /dev/null 2>&1
+/opt/kasm/kasm/build/install/./cuckoo.sh > /dev/null 2>&1
+/opt/kasm/kasm/build/install/./thp.sh > /dev/null 2>&1
+/opt/kasm/kasm/build/install/./tools.sh > /dev/null 2>&1
+sudo apt install postgresql gvm libvirt-daemon -y --fix-missing > /dev/null 2>&1
+sudo -u gvm -g gvm greenbone-nvt-sync > /dev/null 2>&1
+sudo -u gvm -g gvm greenbone-feed-sync --type CERT > /dev/null 2>&1
+sudo -u gvm -g gvm greenbone-feed-sync --type SCAP > /dev/null 2>&1
+sudo -u gvm -g gvm greenbone-feed-sync --type GVMD_DATA > /dev/null 2>&1
 export $(sudo cat /etc/default/gvmd-pg)
-sudo pg_ctlcluster 12 main start
+sudo pg_ctlcluster 12 main start > /dev/null 2>&1
 
 # removing uneeded applications
 printf "\n >> Removing unneeded software...\n"
-sudo du -sh /var/cache/apt/archives # > /dev/null 2>&1
-sudo apt-get remove --auto-remove --purge thunderbird rhythmbox yelp libreoffice* kdeconnect aisleriot gnome-mines gnome-sudoku gnome-mahjongg cheese ghex simple-scan wxhexeditor scite -y # > /dev/null 2>&1
-sudo apt-get autoremove --purge # > /dev/null 2>&1
-sudo apt-get clean # > /dev/null 2>&1
-sudo chmod 777 /etc/sysctl.conf # > /dev/null 2>&1
+sudo du -sh /var/cache/apt/archives > /dev/null 2>&1
+sudo apt-get remove --auto-remove --purge thunderbird rhythmbox yelp libreoffice* kdeconnect aisleriot gnome-mines gnome-sudoku gnome-mahjongg cheese ghex simple-scan wxhexeditor scite -y > /dev/null 2>&1
+sudo apt-get autoremove --purge > /dev/null 2>&1
+sudo apt-get clean > /dev/null 2>&1
 
 # disabling updates via ipv6
+sudo chmod 777 /etc/sysctl.conf
 sudo echo "
 
 net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1
-" >> /etc/sysctl.conf # > /dev/null 2>&1
-sudo chmod 644 /etc/sysctl.conf # > /dev/null 2>&1
+" >> /etc/sysctl.conf
+sudo chmod 644 /etc/sysctl.conf
 printf "\n >>  Uneeded software removed\n"
-python3 -m pip install --upgrade pip # > /dev/null 2>&1
-python2.7 -m pip install --upgrade pip # > /dev/null 2>&1
+python3 -m pip install --upgrade pip > /dev/null 2>&1
+python2.7 -m pip install --upgrade pip > /dev/null 2>&1
 
 # preparing elastic
 printf "\n >>  Preparing elastic stack\n"
-wget -O elastic.py "https://onedrive.live.com/embed?cid=6B2C69CA86AC3FC8&resid=6B2C69CA86AC3FC8%213083290&authkey=ADWrcfFoW6cbo2M" # > /dev/null 2>&1
-sudo mv elastic.py /opt/kasm/kasm/build/install/ # > /dev/null 2>&1
+wget -O elastic.py "https://onedrive.live.com/embed?cid=6B2C69CA86AC3FC8&resid=6B2C69CA86AC3FC8%213083290&authkey=ADWrcfFoW6cbo2M" > /dev/null 2>&1
+sudo mv elastic.py /opt/kasm/kasm/build/install/
 
 # installing apfs-fuse
-sudo apt install libbz2-dev libattr1-dev cmake cmake-curses-gui -y --fix-missing
+sudo apt install libbz2-dev libattr1-dev cmake cmake-curses-gui -y --fix-missing > /dev/null 2>&1
 cd /usr/local/bin
-sudo git clone https://github.com/ezaspy/apfs-fuse.git
+sudo git clone https://github.com/ezaspy/apfs-fuse.git > /dev/null 2>&1
 cd apfs-fuse
 sudo git submodule init
 sudo git submodule update
@@ -92,29 +92,29 @@ sudo make
 # installing virtualisation software
 printf "\n >> Installing virtualisation software\n"
 cd /home/sansforensics/
-sudo wget https://download3.vmware.com/software/WKST-PLAYER-1624/VMware-Player-Full-16.2.4-20089737.x86_64.bundle # > /dev/null 2>&1
-sudo chmod +x VMware-Player-Full-16.2.4-20089737.x86_64.bundle # > /dev/null 2>&1
-sudo ./VMware-Player-Full-16.2.4-20089737.x86_64.bundle # > /dev/null 2>&1
+sudo wget https://download3.vmware.com/software/WKST-PLAYER-1624/VMware-Player-Full-16.2.4-20089737.x86_64.bundle > /dev/null 2>&1
+sudo chmod +x VMware-Player-Full-16.2.4-20089737.x86_64.bundle
+sudo ./VMware-Player-Full-16.2.4-20089737.x86_64.bundle > /dev/null 2>&1
 echo "sudo wget -O virtualbox-7.0_7.0.2-154219~Ubuntu~focal_amd64.deb https://download.virtualbox.org/virtualbox/7.0.2/virtualbox-7.0_7.0.2-154219~Ubuntu~focal_amd64.deb
 sudo dpkg -i virtualbox-7.0_7.0.2-154219~Ubuntu~focal_amd64.deb
-echo virtualbox-ext-pack virtualbox-ext-pack/license select true | sudo debconf-set-selections" > virtualbox.sh # > /dev/null 2>&1
-chmod +x virtualbox.sh # > /dev/null 2>&1
-./virtualbox.sh # > /dev/null 2>&1
-sudo rm -rf VMware-Player-Full-16.2.4-20089737.x86_64.bundle virtualbox.sh virtualbox-7.0_7.0.2-154219~Ubuntu~focal_amd64.deb # > /dev/null 2>&1
+echo virtualbox-ext-pack virtualbox-ext-pack/license select true | sudo debconf-set-selections" > virtualbox.sh
+chmod +x virtualbox.sh
+./virtualbox.sh > /dev/null 2>&1
+sudo rm -rf VMware-Player-Full-16.2.4-20089737.x86_64.bundle virtualbox.sh virtualbox-7.0_7.0.2-154219~Ubuntu~focal_amd64.deb
 
 # initialising elrond
 printf "\n >> Initialising elrond\n"
-sudo git clone https://github.com/ezaspy/elrond.git /opt/elrond # > /dev/null 2>&1
-sudo chmod -R 777 /opt/elrond/elrond/config.sh # > /dev/null 2>&1
-sudo sed -i '7d' /opt/elrond/elrond/config.sh # > /dev/null 2>&1
-sudo sed -i '$ d' /opt/elrond/elrond/config.sh # > /dev/null 2>&1
+sudo git clone https://github.com/ezaspy/elrond.git /opt/elrond > /dev/null 2>&1
+sudo chmod -R 777 /opt/elrond/elrond/config.sh
+sudo sed -i '7d' /opt/elrond/elrond/config.sh
+sudo sed -i '$ d' /opt/elrond/elrond/config.sh
 
 # preparing navigator
-sudo cp /opt/elrond/elrond/rivendell/post/mitre/nav_json.py /opt/kasm/kasm/build/install/nav_json.py # > /dev/null 2>&1
+sudo cp /opt/elrond/elrond/rivendell/post/mitre/nav_json.py /opt/kasm/kasm/build/install/nav_json.py
 
 # installing elrond
-sudo /opt/elrond/./make.sh # > /dev/null 2>&1
-sudo updatedb # > /dev/null 2>&1
+sudo /opt/elrond/./make.sh > /dev/null 2>&1
+sudo updatedb > /dev/null 2>&1
 
 # initialising temp account
 /opt/kasm/kasm/build/./temp.sh
--- kasm/build/ninja.sh
+++ kasm/build/ninja.sh
@@ -1,35 +1,29 @@
 #!/bin/bash
 sudo userdel temp
 sudo rm -rf /home/temp
-clear
-sleep 1
-printf "\n\n  -> Configuring ninja account...\n\n"
-sleep 1
-sudo updatedb
-# clean desktop & set background
+printf "\n >> Configuring ninja account...\n"
+sudo updatedb > /dev/null 2>&1
+
+# clean desktop & set background & set icons
 mkdir /home/ninja/Desktop/Posters
 mv /home/ninja/Desktop/*.pdf /home/ninja/Desktop/Posters
 sudo rm -rf /usr/local/share/remnux/remnux-logo.png
 sudo rm -rf /var/cache/remnux/cli/v2022.44.5/salt-states-2022.44.5/remnux/theme/gnome-config/remnux-logo.png
 sudo cp /opt/kasm/kasm/images/kasm.png /usr/share/backgrounds/kasm.png
 gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/kasm.png
-sleep 1
-# set icons
 gsettings set org.gnome.shell favorite-apps "['gnome-control-center.desktop', 'org.gnome.seahorse.Application.desktop', 'org.gnome.Todo.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Screenshot.desktop', 'google-chrome.desktop', 'firefox.desktop', 'opera_opera.desktop', 'torbrowser.desktop', 'vmware-player.desktop', 'org.gnome.Terminal.desktop', 'code.desktop', 'jupyter-notebook.desktop', 'bless.desktop', 'wireshark.desktop', 'networkminer.desktop', 'cyberchef.desktop', 'die.desktop', 'jd-gui.desktop', 'ghidra.desktop', 'cutter.desktop', 'edb.desktop', 'maltego.desktop']"
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
-sleep 1
+
 # set bash shell theme
-sudo curl -Lo /usr/bin/theme.sh 'https://git.io/JM70M'
+sudo curl -Lo /usr/bin/theme.sh 'https://git.io/JM70M' > /dev/null 2>&1
 sudo chmod +x /usr/bin/theme.sh
 /usr/bin/./theme.sh dracula
-sleep 1
+
 # configure .bashrc
 sudo chmod +x /opt/kasm/kasm/build/ninja.py
 python3 /opt/kasm/kasm/build/ninja.py
-sleep 1
-sudo -E -u gvm -g gvm gvmd --rebuild
-libvirtd -d
-sleep 1
+sudo -E -u gvm -g gvm gvmd --rebuild > /dev/null 2>&1
+libvirtd -d > /dev/null 2>&1
 echo "[Desktop Entry]
 Type=Application
 Exec=gnome-terminal
@@ -44,15 +38,17 @@ sudo rm -rf /home/ninja/.config/autostart/gnome-terminal.desktop
 sudo mv gnome-terminal.desktop /home/ninja/.config/autostart/
 sudo chmod 755 /home/ninja/.config/autostart/gnome-terminal.desktop
 sudo chown -R ninja:ninja /opt/
-sudo dpkg -i /opt/elrond/elrond/tools/.splunk-9.0.0.1-9e907cedecb1-linux-2.6-amd64.deb
+sudo dpkg -i /opt/elrond/elrond/tools/.splunk-9.0.0.1-9e907cedecb1-linux-2.6-amd64.deb > /dev/null 2>&1
 sudo /opt/splunk/bin/./splunk start --accept-license --answer-yes --no-prompt --seed-passwd kasm
-sudo python3 /opt/kasm/kasm/build/install/navigator.py
-sudo python3 /opt/kasm/kasm/build/install/CobaltStrike-Defence.py
+sudo python3 /opt/kasm/kasm/build/install/navigator.py > /dev/null 2>&1
+sudo python3 /opt/kasm/kasm/build/install/CobaltStrike-Defence.py > /dev/null 2>&1
+
 # cleaning repositories
-sudo du -sh /var/cache/apt/archives
-sudo apt-get remove --auto-remove --purge thunderbird rhythmbox yelp libreoffice* aisleriot gnome-mines gnome-sudoku gnome-mahjongg cheese ghex simple-scan -y
-sudo apt-get autoremove --purge
-sudo apt-get clean
+sudo du -sh /var/cache/apt/archives > /dev/null 2>&1
+sudo apt-get remove --auto-remove --purge thunderbird rhythmbox yelp libreoffice* aisleriot gnome-mines gnome-sudoku gnome-mahjongg cheese ghex simple-scan -y > /dev/null 2>&1
+sudo apt-get autoremove --purge > /dev/null 2>&1
+sudo apt-get clean > /dev/null 2>&1
+
 # creating kick-ass.sh
 echo "clear
 sleep 2
@@ -69,10 +65,10 @@ cd /home/ninja/
 " > /home/ninja/Desktop/kick-ass.sh
 sudo chmod +x /home/ninja/Desktop/kick-ass.sh
 sleep 10
-sudo apt install python3-apt
-wget -O aptsources-cleanup.pyz https://github.com/davidfoerster/aptsources-cleanup/releases/download/v0.1.7.5.2/aptsources-cleanup.pyz
+sudo apt install python3-apt > /dev/null 2>&1
+wget -O aptsources-cleanup.pyz https://github.com/davidfoerster/aptsources-cleanup/releases/download/v0.1.7.5.2/aptsources-cleanup.pyz > /dev/null 2>&1
 chmod a+x aptsources-cleanup.pyz
-sudo ./aptsources-cleanup.pyz -n
+sudo ./aptsources-cleanup.pyz -n > /dev/null 2>&1
 history -c
 unset HISTFILE
 sudo su -c "echo 3 >'/proc/sys/vm/drop_caches'
