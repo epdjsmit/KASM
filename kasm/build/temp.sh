db3b893c2863cc73825aa52af12313678b79754c
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-12-05 11:40:40 +0000

    Update temp.sh

--- kasm/build/temp.sh
+++ kasm/build/temp.sh
@@ -106,8 +106,8 @@ sudo mkdir -p /home/ninja/.config/autostart/
 sudo mv gnome-terminal.desktop /home/ninja/.config/autostart/
 sudo chmod 755 /home/ninja/.config/autostart/gnome-terminal.desktop
 sleep 1
-#sudo cp /opt/kasm/kasm/ninja.sh /home/ninja/ninja.sh
-#sudo chmod +x /home/ninja/ninja.sh
+sudo cp /opt/kasm/kasm/ninja.sh /home/ninja/ninja.sh
+sudo chmod +x /home/ninja/ninja.sh
 sudo cp /opt/elrond/elrond/tools/config/scripts/virtual.sh /home/ninja/VMware_Tools.sh
 sudo chmod +x /home/ninja/VMware_Tools.sh
 sleep 1
