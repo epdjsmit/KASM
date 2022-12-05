c2d6ce9f61952949c1b1e9972df9151a964ae7fe
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-12-05 22:12:24 +0000

    Update init.sh

--- kasm/build/init.sh
+++ kasm/build/init.sh
@@ -10,7 +10,8 @@ gsettings set org.gnome.desktop.session idle-delay 0
 # creating cuckoo user
 printf "\n >> Creating Cuckoo user"
 sudo useradd -m -u 6478 -p $(openssl passwd -1 cuckoo) cuckoo && sudo usermod -aG sudo cuckoo > /dev/null 2>&1
-printf "\n  -> Please enter a password for the 'cuckoo' account\n"
+printf "\n  -> Please enter a password for the 'cuckoo' account"
+sleep 1
 sudo passwd cuckoo
 
 # remnux must be installed before anything else
