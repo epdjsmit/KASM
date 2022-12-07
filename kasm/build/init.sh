d5af65375be1bbddfa7be47fc7b122325af5c6bb
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-12-07 10:06:50 +0000

    Update init.sh

--- kasm/build/init.sh
+++ kasm/build/init.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 echo "
-    ##########################################################
-    ---- Commencing KASM-Workstation configuration script ----
-    ##########################################################"
+  ##########################################################
+  ---- Commencing KASM-Workstation configuration script ----
+  ##########################################################"
+sleep 4
+echo "  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ##########################################################"
 sleep 1
-echo "    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    ##########################################################
-    |      Please note the following:                        |
-    |          -> Configuring KASM takes ~10 hours           |
-    |          -> Ensure you have BUILD.md to hand           |
-    |          -> Select the options as instructed           |
-    ##########################################################
+echo "  |      Please note the following:                        |"
+sleep 1
+echo "  |          -> Configuring KASM takes ~10 hours           |"
+sleep 1
+echo "  |          -> Ensure you have BUILD.md to hand           |"
+sleep 1
+echo "  |          -> Select the options as instructed           |"
+sleep 1
+echo "  ##########################################################
 "
-sleep 1
+sleep 10
 # initialising kasm
-printf "\n      >> Initialising KASM-Workstation\n"
+printf "\n    >> Initialising KASM-Workstation\n"
 sudo chmod -R 755 /opt/kasm
 python3 -m keyring --disable
 gsettings set org.gnome.desktop.screensaver lock-enabled false
@@ -22,13 +27,13 @@ gsettings set org.gnome.desktop.lockdown disable-lock-screen true
 gsettings set org.gnome.desktop.session idle-delay 0
 
 # creating cuckoo user
-printf "      >> Creating Cuckoo user\n"
+printf "    >> Creating Cuckoo user\n"
 sudo useradd -m -u 6478 -p $(openssl passwd -1 cuckoo) cuckoo && sudo usermod -aG sudo cuckoo > /dev/null 2>&1
 sleep 1
 sudo passwd cuckoo
 
 # installing apfs-fuse
-printf "\n      >> Installing apfs-fuse\n"
+printf "\n    >> Installing apfs-fuse\n"
 sudo apt install libbz2-dev libattr1-dev cmake cmake-curses-gui -y > /dev/null 2>&1
 cd /usr/local/bin
 sudo git clone https://github.com/ezaspy/apfs-fuse.git > /dev/null 2>&1
@@ -44,18 +49,18 @@ sudo make > /dev/null 2>&1
 # remnux must be installed before anything else
 clear
 echo "
-    ##########################################################
-    ---- Commencing KASM-Workstation configuration script ----
-    ##########################################################"
-echo "    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    ##########################################################
-    |      Please note the following:                        |
-    |          -> Configuring KASM takes ~10 hours           |
-    |          -> Ensure you have BUILD.md to hand           |
-    |          -> Select the options as instructed           |
-    ##########################################################
+  ##########################################################
+  ---- Commencing KASM-Workstation configuration script ----
+  ##########################################################"
+echo "  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ##########################################################
+  |      Please note the following:                        |
+  |          -> Configuring KASM takes ~10 hours           |
+  |          -> Ensure you have BUILD.md to hand           |
+  |          -> Select the options as instructed           |
+  ##########################################################
 "
-printf "\n\n      >> Initialising KASM-Workstation\n      >> Creating Cuckoo user\n\n      >> Installing apfs-fuse\n      >> Installing REMnux\n"
+printf "\n\n    >> Initialising KASM-Workstation\n    >> Creating Cuckoo user\n\n    >> Installing apfs-fuse\n    >> Installing REMnux\n"
 wget https://REMnux.org/remnux-cli > /dev/null 2>&1
 mv remnux-cli remnux
 chmod +x remnux
@@ -63,7 +68,7 @@ sudo mv remnux /usr/local/bin
 sudo remnux install > /dev/null 2>&1
 
 # configuring repositories
-printf "      >> Updating repositories\n"
+printf "    >> Updating repositories\n"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg > /dev/null 2>&1 # thehive
 sleep 4
 yes '' | echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null # thehive / cuckoo
@@ -80,22 +85,22 @@ sudo apt update > /dev/null 2>&1
 
 /opt/kasm/kasm/build/install/./cuckoo.sh > /dev/null 2>&1
 /opt/kasm/kasm/build/install/./thp.sh > /dev/null 2>&1
-printf "      >> Installing Greenbone Vulnerability Manager\n"
+printf "    >> Installing Greenbone Vulnerability Manager\n"
 sudo apt install postgresql libvirt-daemon gvm -y
 clear
 echo "
-    ##########################################################
-    ---- Commencing KASM-Workstation configuration script ----
-    ##########################################################"
-echo "    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    ##########################################################
-    |      Please note the following:                        |
-    |          -> Configuring KASM takes ~10 hours           |
-    |          -> Ensure you have BUILD.md to hand           |
-    |          -> Select the options as instructed           |
-    ##########################################################
+  ##########################################################
+  ---- Commencing KASM-Workstation configuration script ----
+  ##########################################################"
+echo "  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ##########################################################
+  |      Please note the following:                        |
+  |          -> Configuring KASM takes ~10 hours           |
+  |          -> Ensure you have BUILD.md to hand           |
+  |          -> Select the options as instructed           |
+  ##########################################################
 "
-printf "\n      >> Initialising KASM-Workstation\n      >> Creating Cuckoo user\n\n      >> Installing apfs-fuse\n      >> Installing REMnux\n      >> Updating repositories\n      >> Installing Additional Tools\n      >> Installing Greenbone Vulnerability Manager\n"
+printf "\n    >> Initialising KASM-Workstation\n    >> Creating Cuckoo user\n\n    >> Installing apfs-fuse\n    >> Installing REMnux\n    >> Updating repositories\n    >> Installing Additional Tools\n    >> Installing Greenbone Vulnerability Manager\n"
 sudo -u gvm -g gvm greenbone-nvt-sync > /dev/null 2>&1
 sudo -u gvm -g gvm greenbone-feed-sync --type CERT > /dev/null 2>&1
 sudo -u gvm -g gvm greenbone-feed-sync --type SCAP > /dev/null 2>&1
@@ -104,7 +109,7 @@ export $(sudo cat /etc/default/gvmd-pg)
 sudo pg_ctlcluster 12 main start > /dev/null 2>&1
 
 # removing uneeded applications
-printf "      >> Uninstalling redundant software\n"
+printf "    >> Uninstalling redundant software\n"
 sudo du -sh /var/cache/apt/archives > /dev/null 2>&1
 sudo apt-get remove --auto-remove --purge thunderbird rhythmbox yelp libreoffice* kdeconnect aisleriot gnome-mines gnome-sudoku gnome-mahjongg cheese ghex simple-scan wxhexeditor scite -y > /dev/null 2>&1
 sudo apt-get autoremove --purge > /dev/null 2>&1
@@ -114,7 +119,7 @@ sudo apt-get clean > /dev/null 2>&1
 /opt/kasm/kasm/build/install/./tools.sh
 
 # disabling updates via ipv6
-printf "      >> Disabling IPv6 Updates\n"
+printf "    >> Disabling IPv6 Updates\n"
 sudo chmod 777 /etc/sysctl.conf
 sudo echo "
 
@@ -124,17 +129,17 @@ net.ipv6.conf.lo.disable_ipv6 = 1
 " >> /etc/sysctl.conf
 sudo chmod 644 /etc/sysctl.conf
 
-printf "      >> Upgrading Python pip\n"
+printf "    >> Upgrading Python pip\n"
 python3 -m pip install --upgrade pip > /dev/null 2>&1
 python2.7 -m pip install --upgrade pip > /dev/null 2>&1
 
 # preparing elastic
-printf "      >> Preparing elastic stack\n"
+printf "    >> Preparing elastic stack\n"
 wget -O elastic.py "https://onedrive.live.com/embed?cid=6B2C69CA86AC3FC8&resid=6B2C69CA86AC3FC8%213083290&authkey=ADWrcfFoW6cbo2M" > /dev/null 2>&1
 sudo mv elastic.py /opt/kasm/kasm/build/install/
 
 # installing virtualisation software
-printf "      >> Installing virtualisation software\n"
+printf "    >> Installing virtualisation software\n"
 cd /home/sansforensics/
 sudo wget https://download3.vmware.com/software/WKST-PLAYER-1624/VMware-Player-Full-16.2.4-20089737.x86_64.bundle > /dev/null 2>&1
 sudo chmod +x VMware-Player-Full-16.2.4-20089737.x86_64.bundle
@@ -147,7 +152,7 @@ chmod +x virtualbox.sh
 sudo rm -rf VMware-Player-Full-16.2.4-20089737.x86_64.bundle virtualbox.sh virtualbox-7.0_7.0.2-154219~Ubuntu~focal_amd64.deb
 
 # initialising elrond
-printf "      >> Initialising elrond\n"
+printf "    >> Initialising elrond\n"
 sudo git clone https://github.com/ezaspy/elrond.git /opt/elrond > /dev/null 2>&1
 sudo chmod -R 777 /opt/elrond/elrond/config.sh
 sudo sed -i '7d' /opt/elrond/elrond/config.sh
@@ -156,7 +161,7 @@ sudo sed -i '$ d' /opt/elrond/elrond/config.sh
 sudo cp /opt/elrond/elrond/rivendell/post/mitre/nav_json.py /opt/kasm/kasm/build/install/nav_json.py
 
 # installing elrond
-printf "      >> Installing elrond\n"
+printf "    >> Installing elrond\n"
 sudo /opt/elrond/./make.sh > /dev/null 2>&1
 sudo updatedb > /dev/null 2>&1
 
