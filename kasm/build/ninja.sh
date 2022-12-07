4c8c6284063bfd5fb6aa85d57da241f85edc4372
ezaspy <42219366+ezaspy@users.noreply.github.com>
2022-12-07 11:29:48 +0000

    Update ninja.sh

--- kasm/build/ninja.sh
+++ kasm/build/ninja.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 sudo userdel temp
 sudo rm -rf /home/temp
-clear
 sleep 2
-printf "\n >> Configuring 'ninja' account\n"
-sudo updatedb > /dev/null 2>&1
+clear
+echo "
+  ##########################################################
+  ---- Commencing KASM-Workstation configuration script ----
+  ##########################################################"
+echo "  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ##########################################################
+  |      Please note the following:                        |
+  |          -> Configuring KASM takes ~10 hours           |
+  |          -> Ensure you have BUILD.md to hand           |
+  |          -> Select the options as instructed           |
+  ##########################################################
+"
+printf "\n    >> Initialised KASM-Workstation\n    >> Created 'cuckoo' account\n\n    >> Installed apfs-fuse\n    >> Installed REMnux\n    >> Updated repositories\n    >> Installed Additional Tools\n    >> Installed Greenbone Vulnerability Manager\n    >> Uninstalled redundant software\n    >> Installed Chrome Browser\n    >> Installed Opera Browser\n    >> Installed DB Sqlite Browser\n    >> Installed Jupyter Notebooks\n    >> Installed Azure CLI\n    >> Installed AWS CLI\n    >> Installed Bookstack\n    >> Downloaded onestopcybershop\n    >> Downloaded bruce\n    >> Downloaded gandalf\n    >> Downloaded SIGMA\n    >> Downloaded DeepBlueCLI\n    >> Downloaded KAPE\n    >> Downloaded PowerForensics\n    >> Downloaded MemProcFS\n    >> Downloaded WMIExplorer\n    >> Downloaded CobaltStrike-Defence\n    >> Downloaded freq\n    >> Downloaded dnstwist\n    >> Downloaded rdap\n    >> Downloaded sherlock\n    >> Downloaded TweetScraper\n    >> Downloaded karma\n    >> Downloaded TZWorks\n    >> Downloaded BlueTeamPowerShell\n    >> Downloaded Sysmon\n    >> Installed Metasploit\n    >> Installed HTTrack\n    >> Installed Maltego\n    >> Disabled IPv6 Updates\n    >> Upgraded Python pip\n    >> Prepared elastic stack\n    >> Installed virtualisation software\n    >> Initialised elrond\n    >> Installed elrond\n    >> Created 'temp' account\n    >> Configured 'temp' account\n    >> Rebooted\n    >> Configuring 'ninja' account\n"
 
 # clean desktop & set background & set icons
 mkdir /home/ninja/Desktop/Posters
@@ -41,16 +52,9 @@ sudo mv gnome-terminal.desktop /home/ninja/.config/autostart/
 sudo chmod 755 /home/ninja/.config/autostart/gnome-terminal.desktop
 sudo chown -R ninja:ninja /opt/
 sudo dpkg -i /opt/elrond/elrond/tools/.splunk.deb > /dev/null 2>&1
-sudo /opt/splunk/bin/./splunk start --accept-license --answer-yes --no-prompt --seed-passwd kasm
+sudo /opt/splunk/bin/./splunk start --accept-license --answer-yes --no-prompt --seed-passwd kick-ass
 sudo python3 /opt/kasm/kasm/build/install/navigator.py > /dev/null 2>&1
 sudo python3 /opt/kasm/kasm/build/install/CobaltStrike-Defence.py > /dev/null 2>&1
-
-# cleaning repositories
-printf "      >> Finalising KASM\n"
-sudo du -sh /var/cache/apt/archives > /dev/null 2>&1
-sudo apt-get remove --auto-remove --purge thunderbird rhythmbox yelp libreoffice* aisleriot gnome-mines gnome-sudoku gnome-mahjongg cheese ghex simple-scan -y > /dev/null 2>&1
-sudo apt-get autoremove --purge > /dev/null 2>&1
-sudo apt-get clean > /dev/null 2>&1
 
 # creating kick-ass.sh
 echo "clear
@@ -72,10 +76,26 @@ sudo apt install python3-apt > /dev/null 2>&1
 wget -O aptsources-cleanup.pyz https://github.com/davidfoerster/aptsources-cleanup/releases/download/v0.1.7.5.2/aptsources-cleanup.pyz > /dev/null 2>&1
 chmod a+x aptsources-cleanup.pyz
 sudo ./aptsources-cleanup.pyz -n > /dev/null 2>&1
+sudo updatedb > /dev/null 2>&1
 history -c
 unset HISTFILE
-printf "\n      >> Success! KASM has been fully configured\n\n"
+
+sleep 2
+clear
+echo "
+  ##########################################################
+  ---- Commencing KASM-Workstation configuration script ----
+  ##########################################################"
+echo "  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ##########################################################
+  |      Please note the following:                        |
+  |          -> Configuring KASM takes ~10 hours           |
+  |          -> Ensure you have BUILD.md to hand           |
+  |          -> Select the options as instructed           |
+  ##########################################################
+"
+printf "\n    >> Initialised KASM-Workstation\n    >> Created 'cuckoo' account\n\n    >> Installed apfs-fuse\n    >> Installed REMnux\n    >> Updated repositories\n    >> Installed Additional Tools\n    >> Installed Greenbone Vulnerability Manager\n    >> Uninstalled redundant software\n    >> Installed Chrome Browser\n    >> Installed Opera Browser\n    >> Installed DB Sqlite Browser\n    >> Installed Jupyter Notebooks\n    >> Installed Azure CLI\n    >> Installed AWS CLI\n    >> Installed Bookstack\n    >> Downloaded onestopcybershop\n    >> Downloaded bruce\n    >> Downloaded gandalf\n    >> Downloaded SIGMA\n    >> Downloaded DeepBlueCLI\n    >> Downloaded KAPE\n    >> Downloaded PowerForensics\n    >> Downloaded MemProcFS\n    >> Downloaded WMIExplorer\n    >> Downloaded CobaltStrike-Defence\n    >> Downloaded freq\n    >> Downloaded dnstwist\n    >> Downloaded rdap\n    >> Downloaded sherlock\n    >> Downloaded TweetScraper\n    >> Downloaded karma\n    >> Downloaded TZWorks\n    >> Downloaded BlueTeamPowerShell\n    >> Downloaded Sysmon\n    >> Installed Metasploit\n    >> Installed HTTrack\n    >> Installed Maltego\n    >> Disabled IPv6 Updates\n    >> Upgraded Python pip\n    >> Prepared elastic stack\n    >> Installed virtualisation software\n    >> Initialised elrond\n    >> Installed elrond\n    >> Created 'temp' account\n    >> Configured 'temp' account\n    >> Rebooted\n    >> Configured 'ninja' account\n      >> Success! KASM is fully configured\n\n"
 sleep 4
-echo "\n            Press ENTER to perform final reboot"
+echo "\n            Press ENTER to perform Final Reboot"
 read input
 sudo reboot
